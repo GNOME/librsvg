92be36ca7b1f6d4784d22628653fc7366eb1868d
Dom Lachowicz <doml@src.gnome.org>
2004-06-21 22:53:38 +0000

    137934

--- moz-plugin/moz-plugin.c
+++ moz-plugin/moz-plugin.c
@@ -460,9 +460,10 @@ NP_GetValue (void *future, NPPVariable variable, void *value)
 char *
 NP_GetMIMEDescription (void)
 {
-	/* unfortunately, a lot of win32 servers serving up Adobe content return image/svg-xml */
+	/* unfortunately, a lot of win32 servers serving up Adobe content return bogus mime-types... */
 	return ("image/svg+xml:svg:Scalable Vector Graphics;image/svg-xml:svg:Scalable Vector Graphics;"
-			"image/svg:svg:Scalable Vector Graphics");
+			"image/svg:svg:Scalable Vector Graphics;image/vnd.adobe.svg+xml:svg:Scalable Vector Graphics;"
+			"text/xml-svg:svg:Scalable Vector Graphics");
 }
 
 NPError
