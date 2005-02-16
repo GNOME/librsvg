be57dd139c9fd4a89937a2beb11b550835320e9d
Caleb Michael Moore <cmoore@src.gnome.org>
2005-02-16 10:31:40 +0000

    woops, forgot to add rsvg-art-render.c to CVS

---
+++ rsvg-art-render.c
@@ -0,0 +1,60 @@
+/* vim: set sw=4: -*- Mode: C; tab-width: 4; indent-tabs-mode: t; c-basic-offset: 4 -*- */
+/*
+   rsvg-art-render.c: The libart backend plugin
+
+   Copyright (C) 2005 Caleb Moore <c.moore@student.unsw.edu.au>
+
+   This program is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Library General Public License as
+   published by the Free Software Foundation; either version 2 of the
+   License, or (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Library General Public License for more details.
+
+   You should have received a copy of the GNU Library General Public
+   License along with this program; if not, write to the
+   Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+   Boston, MA 02111-1307, USA.
+
+   Authors: Caleb Moore <c.moore@student.unsw.edu.au>
+*/
+
+#include "rsvg-art-composite.h"
+#include "rsvg-art-draw.h"
+#include "rsvg-art-render.h"
+
+RsvgArtRender * rsvg_art_render_new(GdkPixbuf * pb)
+{
+	RsvgArtRender * output;
+	output = g_new(RsvgArtRender, 1);
+
+	output->super.render_path          = rsvg_art_render_path;
+	output->super.pop_discrete_layer   = rsvg_art_pop_discrete_layer;
+	output->super.push_discrete_layer  = rsvg_art_push_discrete_layer;
+
+	output->pixbuf = pb;
+	output->layers = NULL;
+	return output;
+}
+
+static void 
+bogus(RsvgDrawingCtx *ctx)
+{
+}
+
+RsvgArtSVPRender * rsvg_art_svp_render_new()
+{
+	RsvgArtSVPRender * output;
+	output = g_new(RsvgArtSVPRender, 1);
+
+	output->super.render_path          = rsvg_art_svp_render_path;
+	output->super.pop_discrete_layer   = bogus;
+	output->super.push_discrete_layer  = bogus;
+
+	output->outline = NULL;
+	return output;
+}
+
