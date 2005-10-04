f6b986d0f830c0bbfe551a8ba00833438ed248c4
Dom Lachowicz <doml@src.gnome.org>
2005-10-04 12:29:00 +0000

    bug 317615

--- rsvg-shapes.c
+++ rsvg-shapes.c
@@ -147,19 +147,30 @@ _rsvg_node_poly_draw(RsvgNode * self, RsvgDrawingCtx *ctx,
 	RsvgNodePoly * poly = (RsvgNodePoly *)self;
 	gsize i;
 	GString * d;
+	char buf [G_ASCII_DTOSTR_BUF_SIZE];
 
 	/* represent as a "moveto, lineto*, close" path */  
 	if (poly->pointlist_len < 2)
 		return;
 
 	d = g_string_new (NULL);
-	g_string_append_printf (d, "M %f %f ", poly->pointlist[0], poly->pointlist[1] );
-	
-	for (i = 2; i < poly->pointlist_len; i += 2)
-		g_string_append_printf (d, "L %f %f ", poly->pointlist[i], poly->pointlist[i+1]);
-	
+
+	/* 	"M %f %f " */
+	g_string_append (d, " M ");	
+	g_string_append (d, g_ascii_dtostr (buf, sizeof (buf), poly->pointlist[0]));
+	g_string_append_c (d, ' ');
+	g_string_append (d, g_ascii_dtostr (buf, sizeof (buf), poly->pointlist[1]));
+
+	/* "L %f %f " */
+	for (i = 2; i < poly->pointlist_len; i += 2) {
+		g_string_append (d, " L ");	
+		g_string_append (d, g_ascii_dtostr (buf, sizeof (buf), poly->pointlist[i]));
+		g_string_append_c (d, ' ');
+		g_string_append (d, g_ascii_dtostr (buf, sizeof (buf), poly->pointlist[i+1]));
+	}
+
 	if (!poly->is_polyline)
-		g_string_append (d, "Z");
+		g_string_append (d, " Z");
 	
 	rsvg_state_reinherit_top(ctx, self->state, dominate);
 	rsvg_render_path (ctx, d->str);
