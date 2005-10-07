c65b3e9bdc0ffd46351951c1e2ffb6747f217095
Dom Lachowicz <doml@src.gnome.org>
2005-10-07 15:18:16 +0000

    hopefully get rid of static when using the cairo backend

--- rsvg.c
+++ rsvg.c
@@ -134,7 +134,7 @@ static GdkPixbuf * _rsvg_handle_get_pixbuf (RsvgHandle *handle)
 	rsvg_handle_get_dimensions (handle, &dimensions);
 	rowstride = dimensions.width * 4;
 
-	pixels = g_new(guint8, dimensions.width * dimensions.height * 4);
+	pixels = g_new0(guint8, dimensions.width * dimensions.height * 4);
 	surface = cairo_image_surface_create_for_data (pixels,
 												   CAIRO_FORMAT_ARGB32,
 												   dimensions.width, dimensions.height,
