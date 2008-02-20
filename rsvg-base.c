0acabfc0becefdf7c349a80b67c652613d0252bf
Dominic Lachowicz <domlachowicz@gmail.com>
2008-02-20 16:14:35 +0000

    Fixes last bug @ below url. Text inside of text and tspan nodes now gets
    2008-02-20  Dominic Lachowicz <domlachowicz@gmail.com>
    
            * Fixes last bug @ below url. Text inside of text and tspan nodes
            now gets coalesced, so that we can pass it all to a single PangoLayout.
            This fixes some rendering bugs.
    
    
    svn path=/trunk/; revision=1150

--- rsvg-base.c
+++ rsvg-base.c
@@ -704,6 +704,38 @@ rsvg_characters_impl (RsvgHandle * ctx, const xmlChar * ch, int len)
     if (!ch || !len)
         return;
 
+	if (ctx->priv->currentnode)
+		{
+			if (!strcmp ("tspan", ctx->priv->currentnode->type->str) ||
+				!strcmp ("text", ctx->priv->currentnode->type->str))
+				{
+					guint i;
+
+					/* find the last CHARS node in the text or tspan node, so that we
+					   can coalesce the text, and thus avoid screwing up the Pango layouts */
+					self = NULL;
+					for (i = 0; i < ctx->priv->currentnode->children->len; i++) {
+						RsvgNode *node = g_ptr_array_index (ctx->priv->currentnode->children, i);
+						if (!strcmp (node->type->str, "RSVG_NODE_CHARS")) {
+							self = (RsvgNodeChars*)node;
+						}
+					}
+
+					if (self != NULL) {
+						if (!g_utf8_validate ((char *) ch, len, NULL)) {
+							char *utf8;
+							utf8 = rsvg_make_valid_utf8 ((char *) ch, len);
+							g_string_append (self->contents, utf8);
+							g_free (utf8);
+						} else {
+							g_string_append_len (self->contents, (char *)ch, len);
+						}
+
+						return;
+					}
+				}
+		}
+
     self = g_new (RsvgNodeChars, 1);
     _rsvg_node_init (&self->super);
 
