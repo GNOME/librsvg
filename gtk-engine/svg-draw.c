GNOME/librsvg

commit 248b73101710f01711d4da885fb907082a4709e5

Remove deprecated GTK symbol - bug #572084

gtk-engine/svg-draw.c
@@ -594,8 +594,12 @@ draw_string (GtkStyle * style,
 	     gint y,
 	     const gchar * string)
 {
+  PangoLayout *layout = NULL;
+
   g_return_if_fail(style != NULL);
   g_return_if_fail(window != NULL);
+
+  layout = gtk_widget_create_pango_layout (widget, string);
 
   if (state == GTK_STATE_INSENSITIVE)
     {
@@ -605,7 +609,7 @@ draw_string (GtkStyle * style,
 	  gdk_gc_set_clip_rectangle(style->fg_gc[state], area);
 	}
 
-      gdk_draw_string(window, gtk_style_get_font (style), style->fg_gc[state], x, y, string);
+      gdk_draw_layout(GTK_DRAWABLE (window), style->fg_gc[state], x, y, layout);
       
       if (area)
 	{
@@ -616,7 +620,7 @@ draw_string (GtkStyle * style,
   else
     {
       gdk_gc_set_clip_rectangle(style->fg_gc[state], area);
-      gdk_draw_string(window, gtk_style_get_font (style), style->fg_gc[state], x, y, string);
+      gdk_draw_layout(GTK_DRAWABLE (window), style->fg_gc[state], x, y, layout);
       gdk_gc_set_clip_rectangle(style->fg_gc[state], NULL);
     }
 }

commit 95eaad11b6f3e3d85ea9375e26db948b2224511a

Use GDK_DRAWABLE() instead of non-existing GTK_DRAWABLE()
The error was introduced in commit 248b7310.

gtk-engine/svg-draw.c
@@ -609,7 +609,7 @@ draw_string (GtkStyle * style,
 	  gdk_gc_set_clip_rectangle(style->fg_gc[state], area);
 	}
 
-      gdk_draw_layout(GTK_DRAWABLE (window), style->fg_gc[state], x, y, layout);
+      gdk_draw_layout(GDK_DRAWABLE (window), style->fg_gc[state], x, y, layout);
       
       if (area)
 	{
@@ -620,7 +620,7 @@ draw_string (GtkStyle * style,
   else
     {
       gdk_gc_set_clip_rectangle(style->fg_gc[state], area);
-      gdk_draw_layout(GTK_DRAWABLE (window), style->fg_gc[state], x, y, layout);
+      gdk_draw_layout(GDK_DRAWABLE (window), style->fg_gc[state], x, y, layout);
       gdk_gc_set_clip_rectangle(style->fg_gc[state], NULL);
     }
 }