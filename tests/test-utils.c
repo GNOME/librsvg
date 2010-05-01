65c43f4772ec16e3f984abe80b05aad6f097cc9a
Hiroyuki Ikezoe <hiikezoe@gnome.org>
2010-05-01 10:24:19 +0900

    set correct fixture path.

--- tests/test-utils.c
+++ tests/test-utils.c
@@ -8,25 +8,13 @@ static gchar *data_path = NULL;
 const gchar *
 test_utils_get_test_data_path (void)
 {
-    gchar *prgname;
-    gchar *dirname;
-
     if (data_path)
         return data_path;
 
-    prgname = g_get_prgname ();
-    dirname = g_path_get_dirname (prgname);
-
-#ifdef LT_OBJDIR
-    {
-        gchar *another_dirname;
-        another_dirname = g_path_get_dirname (dirname);
-        g_free (dirname);
-        dirname = another_dirname;
-    }
-#endif
-    data_path = g_build_filename (dirname, "fixtures", NULL);
-    g_free (dirname);
+    data_path = g_build_filename (TEST_SRC_DIR,
+                                  TOP_SRC_DIR,
+                                  "tests",
+                                  "fixtures", NULL);
 
     return data_path;
 }
