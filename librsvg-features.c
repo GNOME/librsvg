f7ad04b70761152aefee15109d5d92a87d0e9054
Christian Persch <chpe@gnome.org>
2012-06-19 14:05:37 +0200

    Tidy code

--- librsvg-features.c
+++ librsvg-features.c
@@ -1,7 +1,7 @@
 #include "rsvg.h"
 
 /* General initialization hooks */
-const unsigned int librsvg_major_version = LIBRSVG_MAJOR_VERSION,
-    librsvg_minor_version = LIBRSVG_MINOR_VERSION, librsvg_micro_version = LIBRSVG_MICRO_VERSION;
-
-const char *librsvg_version = LIBRSVG_VERSION;
+const guint librsvg_major_version = LIBRSVG_MAJOR_VERSION;
+const guint librsvg_minor_version = LIBRSVG_MINOR_VERSION;
+const guint librsvg_micro_version = LIBRSVG_MICRO_VERSION;
+const char librsvg_version[] = LIBRSVG_VERSION;
