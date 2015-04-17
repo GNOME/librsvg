3717bbc32da2165f37213727fac4431a64507e21
Federico Mena Quintero <federico@gnome.org>
2015-04-17 10:24:38 -0500

    test-display: Don't use deprecated gtk_scrolled_window_add_with_viewport()

--- test-display.c
+++ test-display.c
@@ -533,8 +533,7 @@ populate_window (GtkWidget * win,
     scroll = gtk_scrolled_window_new (NULL, NULL);
     gtk_scrolled_window_set_policy (GTK_SCROLLED_WINDOW (scroll),
                                     GTK_POLICY_AUTOMATIC, GTK_POLICY_AUTOMATIC);
-    gtk_scrolled_window_add_with_viewport (GTK_SCROLLED_WINDOW (scroll), 
-                                           GTK_WIDGET (info->image));
+    gtk_container_add (GTK_CONTAINER (scroll), GTK_WIDGET (info->image));
     gtk_box_pack_start (GTK_BOX (vbox), scroll, TRUE, TRUE, 0);
 }
 
