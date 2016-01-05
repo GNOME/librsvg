1fc06fe6def169920f4032e348040c38be320fb1
Menner <mik@gmx.org>
2016-01-05 13:50:17 -0600

    rsvg-marker: Don't try to render NULL markers
    https://bugzilla.gnome.org/show_bug.cgi?id=685906#c7

--- rsvg-marker.c
+++ rsvg-marker.c
@@ -109,6 +109,9 @@ rsvg_marker_render (const char * marker_name, gdouble xpos, gdouble ypos, gdoubl
     gdouble rotation;
     RsvgState *state = rsvg_current_state (ctx);
 
+    if (marker_name == NULL)
+        return; /* to avoid the caller having to check for nonexistent markers on every call */
+
     self = (RsvgMarker *) rsvg_acquire_node (ctx, marker_name);
     if (self == NULL || RSVG_NODE_TYPE (&self->super) != RSVG_NODE_TYPE_MARKER)
       {
