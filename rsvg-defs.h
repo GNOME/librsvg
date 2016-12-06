f732406e73427af610e587f66f58f532617cbd0b
Federico Mena Quintero <federico@gnome.org>
2016-12-05 18:49:49 -0600

    Remove unused prototype

--- rsvg-defs.h
+++ rsvg-defs.h
@@ -43,8 +43,6 @@ G_GNUC_INTERNAL
 void	     rsvg_defs_free		(RsvgDefs * defs);
 G_GNUC_INTERNAL
 void	     rsvg_defs_register_node_by_id (RsvgDefs *defs, const char *id, RsvgNode *node);
-G_GNUC_INTERNAL
-void	     rsvg_defs_register_memory  (RsvgDefs * defs, RsvgNode * val);
 
 G_END_DECLS
 #endif
