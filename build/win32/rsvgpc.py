bee15f05e9ef78f3c624590db4abd691129077c0
Chun-wei Fan <fanchunwei@src.gnome.org>
2017-03-03 17:19:43 +0800

    Visual Studio builds: Add scripts to generate .pc files
    Integration into the Visual Studio process will follow, this uses Python
    to generate librsvg-2.0.pc.

---
+++ build/win32/rsvgpc.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python
+#
+# Utility script to generate .pc files for GLib
+# for Visual Studio builds, to be used for
+# building introspection files
+
+# Author: Fan, Chun-wei
+# Date: March 10, 2016
+
+import os
+import sys
+
+from replace import replace_multi
+from pc_base import BasePCItems
+
+def main(argv):
+    rsvg_api_ver = '2.0'
+    base_pc = BasePCItems()
+
+    base_pc.setup(argv)
+    pkg_replace_items = {'@RSVG_API_MAJOR_VERSION@': rsvg_api_ver,
+                         '@RSVG_API_VERSION@': rsvg_api_ver,
+                         ' -lm': ''}
+
+    pkg_replace_items.update(base_pc.base_replace_items)
+
+    # Generate librsvg-$(rsvg_api_ver).pc
+    replace_multi(base_pc.top_srcdir + '/librsvg.pc.in',
+                  base_pc.srcdir + '/librsvg-' + rsvg_api_ver + '.pc',
+                  pkg_replace_items)
+
+if __name__ == '__main__':
+    sys.exit(main(sys.argv))
