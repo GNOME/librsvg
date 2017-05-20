267e6f284380461da248b9898b82eeaaea956478
Federico Mena Quintero <federico@gnome.org>
2017-05-19 20:21:01 -0500

    color.rs: Convert directly from cssparser's Result to our ColorSpec

--- rust/src/color.rs
+++ rust/src/color.rs
@@ -1,8 +1,3 @@
-use std::str::FromStr;
-
-use parsers::ParseError;
-use error::*;
-
 use ::cssparser;
 
 // Keep this in sync with rsvg-css.h:RsvgCssColorKind
@@ -23,59 +18,29 @@ pub struct ColorSpec {
     argb: u32
 }
 
-impl From<cssparser::Color> for ColorSpec {
-    fn from (color: cssparser::Color) -> ColorSpec {
-        match color {
-            cssparser::Color::CurrentColor =>
+impl<'i> From<Result<cssparser::Color, cssparser::BasicParseError<'i>>> for ColorSpec {
+    fn from (result: Result<cssparser::Color, cssparser::BasicParseError<'i>>) -> ColorSpec {
+        match result {
+            Ok (cssparser::Color::CurrentColor) =>
                 ColorSpec {
                     kind: ColorKind::CurrentColor,
                     argb: 0
                 },
 
-            cssparser::Color::RGBA (rgba) =>
+            Ok (cssparser::Color::RGBA (rgba)) =>
                 ColorSpec {
                     kind: ColorKind::ARGB,
                     argb: ((rgba.alpha as u32) << 24 |
                            (rgba.red as u32)   << 16 |
                            (rgba.green as u32) << 8  |
                            (rgba.blue as u32))
+                },
+
+            _ =>
+                ColorSpec {
+                    kind: ColorKind::ParseError,
+                    argb: 0
                 }
-        }
-    }
-}
-
-#[derive(Debug, Copy, Clone, PartialEq, Eq)]
-pub struct RawColor {
-    pub argb: u32
-}
-
-impl RawColor {
-    pub fn new_rgb (r: u8, g: u8, b: u8) -> RawColor {
-        RawColor {
-            argb: (0xff000000       |
-                   (r as u32) << 16 |
-                   (g as u32) << 8  |
-                   (b as u32))
-        }
-    }
-
-    pub fn new_argb (a: u8, r: u8, g: u8, b: u8) -> RawColor {
-        RawColor {
-            argb: ((a as u32) << 24 |
-                   (r as u32) << 16 |
-                   (g as u32) << 8  |
-                   (b as u32))
-        }
-    }
-}
-
-impl FromStr for RawColor {
-    type Err = AttributeError;
-
-    fn from_str (s: &str) -> Result<RawColor, AttributeError> {
-        match cssparser::Color::parse (&mut cssparser::Parser::new (s)) {
-            Ok (cssparser::Color::RGBA (rgba)) => Ok (RawColor::new_argb (rgba.alpha, rgba.red, rgba.green, rgba.blue)),
-            _ => Err (AttributeError::Parse (ParseError::new ("invalid color specification")))
         }
     }
 }
@@ -85,72 +50,54 @@ mod tests {
     use super::*;
     use std::str::FromStr;
 
+    fn parse (s: &str) -> ColorSpec {
+        ColorSpec::from (cssparser::Color::parse (&mut cssparser::Parser::new (s)))
+    }
+
     #[test]
     fn parses_hash_hex_colors () {
-        assert_eq! (RawColor::from_str ("#AB10fa20").unwrap (), RawColor::new_argb (0x20, 0xab, 0x10, 0xfa));
-        assert_eq! (RawColor::from_str ("#10fa20").unwrap (),   RawColor::new_rgb  (0x10, 0xfa, 0x20));
-        assert_eq! (RawColor::from_str ("#abcd").unwrap (),     RawColor::new_argb (0xdd, 0xaa, 0xbb, 0xcc));
-        assert_eq! (RawColor::from_str ("#123").unwrap (),      RawColor::new_rgb  (0x11, 0x22, 0x33));
+        assert_eq! (parse ("#AB10fa20"), ColorSpec { kind: ColorKind::ARGB, argb: 0x20ab10fa });
+        assert_eq! (parse ("#10fa20"),   ColorSpec { kind: ColorKind::ARGB, argb: 0xff10fa20 });
+        assert_eq! (parse ("#abcd"),     ColorSpec { kind: ColorKind::ARGB, argb: 0xddaabbcc });
+        assert_eq! (parse ("#123"),      ColorSpec { kind: ColorKind::ARGB, argb: 0xff112233 });
     }
 
     #[test]
     fn parses_color_keywords () {
-        assert_eq! (RawColor::from_str ("red").unwrap (),  RawColor::new_rgb (0xff, 0x00, 0x00));
-        assert_eq! (RawColor::from_str ("lime").unwrap (), RawColor::new_rgb (0x00, 0xff, 0x00));
-        assert_eq! (RawColor::from_str ("blue").unwrap (), RawColor::new_rgb (0x00, 0x00, 0xff));
+        assert_eq! (parse ("red"),  ColorSpec { kind: ColorKind::ARGB, argb: 0xffff0000 });
+        assert_eq! (parse ("lime"), ColorSpec { kind: ColorKind::ARGB, argb: 0xff00ff00 });
+        assert_eq! (parse ("blue"), ColorSpec { kind: ColorKind::ARGB, argb: 0xff0000ff });
     }
 
     #[test]
     fn parses_color_functions () {
-        assert_eq! (RawColor::from_str ("rgb(255, 0, 0)").unwrap (), RawColor::new_rgb (0xff, 0x00, 0x00));
-        assert_eq! (RawColor::from_str ("rgb(0, 255, 0)").unwrap (), RawColor::new_rgb (0x00, 0xff, 0x00));
-        assert_eq! (RawColor::from_str ("rgb(0, 0, 255)").unwrap (), RawColor::new_rgb (0x00, 0x00, 0xff));
+        assert_eq! (parse ("rgb(255, 0, 0)"), ColorSpec { kind: ColorKind::ARGB, argb: 0xffff0000 });
+        assert_eq! (parse ("rgb(0, 255, 0)"), ColorSpec { kind: ColorKind::ARGB, argb: 0xff00ff00 });
+        assert_eq! (parse ("rgb(0, 0, 255)"), ColorSpec { kind: ColorKind::ARGB, argb: 0xff0000ff });
     }
 
     #[test]
-    fn invalid_hash_hex_colors_yield_error () {
-        assert! (RawColor::from_str ("#").is_err ());
-        assert! (RawColor::from_str ("#xyz").is_err ());
-        assert! (RawColor::from_str ("#112233gg").is_err ());
+    fn parses_current_color () {
+        assert_eq! (parse ("currentColor"), ColorSpec { kind: ColorKind::CurrentColor, argb: 0 });
     }
 
-    #[test]
-    fn invalid_colors_yield_error () {
-        assert! (RawColor::from_str ("").is_err ());
-        assert! (RawColor::from_str ("foo").is_err ());
-    }
-
-    fn make_color (a: u8, r: u8, g: u8, b: u8) -> cssparser::Color {
-        cssparser::Color::RGBA (cssparser::RGBA::new (r, g, b, a))
-    }
-
-    fn make_color_spec (a: u8, r: u8, g: u8, b: u8) -> ColorSpec {
+    fn make_error () -> ColorSpec {
         ColorSpec {
-            kind: ColorKind::ARGB,
-            argb: ((a as u32) << 24 |
-                   (r as u32) << 16 |
-                   (g as u32) << 8  |
-                   (b as u32))
-        }
-    }
-
-    fn make_color_spec_current_color () -> ColorSpec {
-        ColorSpec {
-            kind: ColorKind::CurrentColor,
+            kind: ColorKind::ParseError,
             argb: 0
         }
     }
 
     #[test]
-    fn color_spec_from_css_color () {
-        assert_eq! (ColorSpec::from (make_color (255, 0, 0, 0)), make_color_spec (255, 0, 0, 0));
-        assert_eq! (ColorSpec::from (make_color (0, 255, 0, 0)), make_color_spec (0, 255, 0, 0));
-        assert_eq! (ColorSpec::from (make_color (0, 0, 255, 0)), make_color_spec (0, 0, 255, 0));
-        assert_eq! (ColorSpec::from (make_color (0, 0, 0, 255)), make_color_spec (0, 0, 0, 255));
+    fn invalid_hash_hex_colors_yield_error () {
+        assert_eq! (parse ("#"), make_error ());
+        assert_eq! (parse ("#xyz"), make_error ());
+        assert_eq! (parse ("#112233gg"), make_error ());
     }
 
     #[test]
-    fn color_spec_from_css_current_color () {
-        assert_eq! (ColorSpec::from (cssparser::Color::CurrentColor), make_color_spec_current_color ());
+    fn invalid_colors_yield_error () {
+        assert_eq! (parse (""), make_error ());
+        assert_eq! (parse ("foo"), make_error ());
     }
 }
