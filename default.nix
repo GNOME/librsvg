5d899f38e9164d320181936b757d1dad56e0503d
Stefan Lau <github@stefanlau.com>
2017-11-27 20:26:50 +0100

    Update rust in default.nix for bitflags dependency

--- default.nix
+++ default.nix
@@ -2,9 +2,9 @@ let
     host_pkgs = import <nixpkgs> {};
     nixpkgs = host_pkgs.fetchFromGitHub {
         owner = "NixOS";
-        repo = "nixpkgs";
-        rev = "6cf604828568f0594db75daae39498f055c1a01f";
-        sha256 = "1crmrvlwkvixq3m1xn2xzy5j72gxmgillpzg6i2wkf67mmzdi1r7";
+        repo = "nixpkgs-channels";
+        rev = "45a85eacebf2181d2e12c0c1005bf3ba07583a74";
+        sha256 = "1ksrjl897cq6j2b3h0s671sd626fbbf6sq87yvrl26wvyrrd7dz8";
     };
 in
 with import nixpkgs {};
