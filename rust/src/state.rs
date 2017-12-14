a4acff186ee43451bceb1f19b4f1832b4f60b3b7
Federico Mena Quintero <federico@gnome.org>
2017-12-14 14:16:49 -0600

    state.rs: Bind rsvg_state_get_language() to Rust

--- rust/src/state.rs
+++ rust/src/state.rs
@@ -1,1 +1,14 @@
+use libc;
+use glib::translate::*;
+
 pub enum RsvgState {}
+
+extern "C" {
+    fn rsvg_state_get_language(state: *const RsvgState) -> *const libc::c_char;
+}
+
+pub fn get_language(state: *const RsvgState) -> Option<String> {
+    unsafe {
+        from_glib_none(rsvg_state_get_language(state))
+    }
+}
