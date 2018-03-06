855acd74b53960493e282187b51aaa9147da9aa8
Federico Mena Quintero <federico@gnome.org>
2018-03-06 13:47:14 -0600

    util.rs: Remove DBL_EPSILON and double_equals().  Yay!

--- rsvg_internals/src/util.rs
+++ rsvg_internals/src/util.rs
@@ -14,12 +14,6 @@ pub fn clone_fallback_name(fallback: &Option<String>) -> Option<String> {
     }
 }
 
-const DBL_EPSILON: f64 = 1e-10;
-
-pub fn double_equals(a: f64, b: f64) -> bool {
-    (a - b).abs() < DBL_EPSILON
-}
-
 /// Converts a `char *` which is known to be valid UTF-8 into a `&str`
 ///
 /// The usual `from_glib_none(s)` allocates an owned String.  The
