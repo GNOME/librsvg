ed2a9567424025a29bf85c9f1ff5344df00e0fdd
Federico Mena Quintero <federico@gnome.org>
2018-03-06 12:57:19 -0600

    util.rs: Make DBL_EPSILON private; we'll remove it soon

--- rsvg_internals/src/util.rs
+++ rsvg_internals/src/util.rs
@@ -14,7 +14,7 @@ pub fn clone_fallback_name(fallback: &Option<String>) -> Option<String> {
     }
 }
 
-pub const DBL_EPSILON: f64 = 1e-10;
+const DBL_EPSILON: f64 = 1e-10;
 
 pub fn double_equals(a: f64, b: f64) -> bool {
     (a - b).abs() < DBL_EPSILON
