105ecdf46c34b6ee69674c7ddc0129654d811f9d
Federico Mena Quintero <federico@gnome.org>
2018-03-29 09:41:25 -0600

    Remove RsvgCairoClipRender; just use the current RsvgCairoRender
    At this point, RsvgCairoClipRender was just a way to have a rendering
    loop run (e.g. render a subtree of elements, but to build a clipping
    path instead of normal rendering) without accumulating bounding boxes:
    RsvgCairoClipRender had its own accumulator (the inherited ->bbox
    field), but just ignored it after the clipping was done.
    
    So, we just remove RsvgCairoClipRender and overwrite the bbox field in
    the original RsvgCairoRender *with its original value* before we ran
    the clipping loop.
    
    We will be able to get rid of this when we can extract bounding boxes
    outside of a full-blown rendering loop.

--- librsvg/rsvg-cairo-clip.c
+++ librsvg/rsvg-cairo-clip.c
@@ -40,69 +40,23 @@
 #include <string.h>
 #include <pango/pangocairo.h>
 
-typedef struct RsvgCairoClipRender RsvgCairoClipRender;
-
-struct RsvgCairoClipRender {
-    RsvgCairoRender super;
-};
-
-#define RSVG_CAIRO_CLIP_RENDER(render) (_RSVG_RENDER_CIC ((render), RSVG_RENDER_TYPE_CAIRO_CLIP, RsvgCairoClipRender))
-
-static void
-rsvg_cairo_clip_render_free (RsvgRender * self)
-{
-    RsvgCairoClipRender *clip_render = RSVG_CAIRO_CLIP_RENDER (self);
-
-    g_free (clip_render);
-}
-
-static RsvgRender *
-rsvg_cairo_clip_render_new (cairo_t *cr, RsvgCairoRender *parent)
-{
-    RsvgCairoClipRender *clip_render = g_new0 (RsvgCairoClipRender, 1);
-    RsvgCairoRender *cairo_render = &clip_render->super;
-    RsvgRender *render = &cairo_render->super;
-
-    g_assert (parent->super.type == RSVG_RENDER_TYPE_CAIRO);
-
-    render->type = RSVG_RENDER_TYPE_CAIRO_CLIP;
-    render->free = rsvg_cairo_clip_render_free;
-
-    cairo_render->initial_cr = parent->initial_cr;
-    cairo_render->cr         = cr;
-    cairo_render->width      = parent->width;
-    cairo_render->height     = parent->height;
-    cairo_render->offset_x   = parent->offset_x;
-    cairo_render->offset_y   = parent->offset_y;
-    cairo_render->cr_stack   = NULL;
-    cairo_render->bbox       = parent->bbox;
-    cairo_render->bb_stack   = NULL;
-
-    /* We don't copy or ref the following two; we just share them */
-#ifdef HAVE_PANGO_FT2
-    cairo_render->font_config_for_testing = parent->font_config_for_testing;
-    cairo_render->font_map_for_testing    = parent->font_map_for_testing;
-#endif
-
-    return render;
-}
-
 void
 rsvg_cairo_clip (RsvgDrawingCtx * ctx, RsvgNode *node_clip_path, RsvgBbox * bbox)
 {
-    RsvgCairoClipRender *clip_render;
     RsvgCairoRender *save = RSVG_CAIRO_RENDER (ctx->render);
     cairo_matrix_t affinesave;
     RsvgState *clip_path_state;
     cairo_t *cr;
     RsvgCoordUnits clip_units;
+    GList *orig_cr_stack;
+    GList *orig_bb_stack;
+    GList *orig_surfaces_stack;
+    RsvgBbox orig_bbox;
 
     g_assert (rsvg_node_get_type (node_clip_path) == RSVG_NODE_TYPE_CLIP_PATH);
     clip_units = rsvg_node_clip_path_get_units (node_clip_path);
 
     cr = save->cr;
-    clip_render = RSVG_CAIRO_CLIP_RENDER (rsvg_cairo_clip_render_new (cr, save));
-    ctx->render = &clip_render->super.super;
 
     clip_path_state = rsvg_node_get_state (node_clip_path);
 
@@ -121,6 +75,12 @@ rsvg_cairo_clip (RsvgDrawingCtx * ctx, RsvgNode *node_clip_path, RsvgBbox * bbox
         rsvg_state_set_affine (clip_path_state, bbtransform);
     }
 
+    orig_cr_stack = save->cr_stack;
+    orig_bb_stack = save->bb_stack;
+    orig_surfaces_stack = save->surfaces_stack;
+
+    orig_bbox = save->bbox;
+
     rsvg_state_push (ctx);
     rsvg_node_draw_children (node_clip_path, ctx, 0, TRUE);
     rsvg_state_pop (ctx);
@@ -129,11 +89,16 @@ rsvg_cairo_clip (RsvgDrawingCtx * ctx, RsvgNode *node_clip_path, RsvgBbox * bbox
         rsvg_state_set_affine (clip_path_state, affinesave);
     }
 
-    g_assert (clip_render->super.cr_stack == NULL);
-    g_assert (clip_render->super.bb_stack == NULL);
-    g_assert (clip_render->super.surfaces_stack == NULL);
+    g_assert (save->cr_stack == orig_cr_stack);
+    g_assert (save->bb_stack == orig_bb_stack);
+    g_assert (save->surfaces_stack == orig_surfaces_stack);
 
-    g_free (ctx->render);
+    /* FIXME: this is an EPIC HACK to keep the clipping context from
+     * accumulating bounding boxes.  We'll remove this later, when we
+     * are able to extract bounding boxes from outside the
+     * general drawing loop.
+     */
+    save->bbox = orig_bbox;
+
     cairo_clip (cr);
-    ctx->render = &save->super;
 }
