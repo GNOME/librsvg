3a46343abb84cac62e8a191263842ded88069326
Ivan Molodetskikh <yalterz@gmail.com>
2018-05-26 11:41:55 +0300

    Fix rounding in FilterContext::compute_bounds()

--- rsvg_internals/src/filter_context.rs
+++ rsvg_internals/src/filter_context.rs
@@ -252,10 +252,10 @@ impl FilterContext {
 
         let bbox_rect = bbox.rect.unwrap();
         IRect {
-            x0: bbox_rect.x as i32,
-            y0: bbox_rect.y as i32,
-            x1: (bbox_rect.x + bbox_rect.width) as i32,
-            y1: (bbox_rect.y + bbox_rect.height) as i32,
+            x0: bbox_rect.x.floor() as i32,
+            y0: bbox_rect.y.floor() as i32,
+            x1: (bbox_rect.x + bbox_rect.width).ceil() as i32,
+            y1: (bbox_rect.y + bbox_rect.height).ceil() as i32,
         }
     }
 }
