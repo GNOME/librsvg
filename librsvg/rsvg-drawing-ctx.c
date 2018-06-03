478ba5ca392d8561baa4f302c1e9c6e0890eff98
Paolo Borelli <pborelli@gnome.org>
2018-06-03 19:53:11 +0200

    drawing_ctx: move get_acquired_node_of_type to rust

--- librsvg/rsvg-drawing-ctx.c
+++ librsvg/rsvg-drawing-ctx.c
@@ -265,44 +265,6 @@ rsvg_drawing_ctx_acquire_node (RsvgDrawingCtx *ctx, const char *url)
   return node;
 }
 
-/**
- * rsvg_drawing_ctx_acquire_node_of_type:
- * @ctx: The drawing context in use
- * @url: The IRI to lookup
- * @type: Type which the node must have
- *
- * Use this function when looking up urls to other nodes, and when you expect
- * the node to be of a particular type. This function does proper recursion
- * checking and thereby avoids infinite loops.
- *
- * Malformed SVGs, for example, may reference a marker by its IRI, but
- * the object referenced by the IRI is not a marker.
- *
- * Nodes acquired by this function must be released using
- * rsvg_drawing_ctx_release_node() in reverse acquiring order.
- *
- * Note that if you acquire a node, you have to release it before trying to
- * acquire it again.  If you acquire a node "#foo" and don't release it before
- * trying to acquire "foo" again, you will obtain a %NULL the second time.
- *
- * Returns: The node referenced by @url or %NULL if the @url
- *          does not reference a node.  Also returns %NULL if
- *          the node referenced by @url is not of the specified @type.
- */
-RsvgNode *
-rsvg_drawing_ctx_acquire_node_of_type (RsvgDrawingCtx *ctx, const char *url, RsvgNodeType type)
-{
-    RsvgNode *node;
-
-    node = rsvg_drawing_ctx_acquire_node (ctx, url);
-    if (node == NULL || rsvg_node_get_type (node) != type) {
-        rsvg_drawing_ctx_release_node (ctx, node);
-        return NULL;
-    }
-
-    return node;
-}
-
 /*
  * rsvg_drawing_ctx_release_node:
  * @ctx: The drawing context the node was acquired from
