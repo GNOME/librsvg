81b118e915d319fbf1c5fe1b2b4a1b1dace8beb9
Federico Mena Quintero <federico@gnome.org>
2018-06-19 15:38:19 -0500

    viewport.rs: Inline everything into draw_in_viewport(); remove the tests (!)
    The trait we used for mocking the RsvgDrawingCtx in the tests was
    playing badly with the efforts to replace push/pop_discrete_layer()
    with a single with_discrete_layer() function.
    
    In the end I want to move to a model where RsvgDrawingCtx isn't
    essentially a globally mutable mess, and intead functions pass around
    a current Viewport that knows how to expose a cairo::Context and
    things like the viewport size.
    
    The tests here are removed, but they were essentially testing that we
    call preserve_aspect_ratio() with the correct values.  I hope to have
    more fine-grained tests in the future.

--- rsvg_internals/src/viewport.rs
+++ rsvg_internals/src/viewport.rs
@@ -26,83 +26,8 @@ pub fn draw_in_viewport<F>(
     preserve_aspect_ratio: AspectRatio,
     node: &RsvgNode,
     values: &ComputedValues,
-    affine: cairo::Matrix,
-    draw_ctx: *mut RsvgDrawingCtx,
-    clipping: bool,
-    draw_fn: F,
-) where
-    F: FnOnce(),
-{
-    let mut ctx = RsvgDrawingCtxWrapper(draw_ctx);
-
-    in_viewport(
-        &mut ctx,
-        vx,
-        vy,
-        vw,
-        vh,
-        clip_mode,
-        do_clip,
-        vbox,
-        preserve_aspect_ratio,
-        node,
-        values,
-        affine,
-        clipping,
-        draw_fn,
-    );
-}
-
-trait ViewportCtx {
-    fn push_view_box(&mut self, width: f64, height: f64);
-    fn pop_view_box(&mut self);
-    fn push_discrete_layer(&mut self, values: &ComputedValues, clipping: bool);
-    fn pop_discrete_layer(&mut self, node: &RsvgNode, values: &ComputedValues, clipping: bool);
-    fn add_clipping_rect(&mut self, x: f64, y: f64, w: f64, h: f64);
-    fn set_affine(&mut self, affine: cairo::Matrix);
-}
-
-struct RsvgDrawingCtxWrapper(pub *mut RsvgDrawingCtx);
-
-impl ViewportCtx for RsvgDrawingCtxWrapper {
-    fn push_view_box(&mut self, width: f64, height: f64) {
-        drawing_ctx::push_view_box(self.0, width, height);
-    }
-
-    fn pop_view_box(&mut self) {
-        drawing_ctx::pop_view_box(self.0);
-    }
-
-    fn push_discrete_layer(&mut self, values: &ComputedValues, clipping: bool) {
-        drawing_ctx::push_discrete_layer(self.0, values, clipping);
-    }
-
-    fn pop_discrete_layer(&mut self, node: &RsvgNode, values: &ComputedValues, clipping: bool) {
-        drawing_ctx::pop_discrete_layer(self.0, node, values, clipping);
-    }
-
-    fn add_clipping_rect(&mut self, x: f64, y: f64, w: f64, h: f64) {
-        add_clipping_rect(self.0, x, y, w, h);
-    }
-
-    fn set_affine(&mut self, affine: cairo::Matrix) {
-        drawing_ctx::get_cairo_context(self.0).set_matrix(affine);
-    }
-}
-
-fn in_viewport<F>(
-    ctx: &mut ViewportCtx,
-    vx: f64,
-    vy: f64,
-    vw: f64,
-    vh: f64,
-    clip_mode: ClipMode,
-    do_clip: bool,
-    vbox: Option<ViewBox>,
-    preserve_aspect_ratio: AspectRatio,
-    node: &RsvgNode,
-    values: &ComputedValues,
     mut affine: cairo::Matrix,
+    draw_ctx: *mut RsvgDrawingCtx,
     clipping: bool,
     draw_fn: F,
 ) where
@@ -118,11 +43,11 @@ fn in_viewport<F>(
         return;
     }
 
-    ctx.push_discrete_layer(values, clipping);
+    drawing_ctx::push_discrete_layer(draw_ctx, values, clipping);
 
     if do_clip && clip_mode == ClipMode::ClipToViewport {
-        ctx.set_affine(affine);
-        ctx.add_clipping_rect(vx, vy, vw, vh);
+        drawing_ctx::get_cairo_context(draw_ctx).set_matrix(affine);
+        add_clipping_rect(draw_ctx, vx, vy, vw, vh);
     }
 
     if let Some(vbox) = vbox {
@@ -135,7 +60,7 @@ fn in_viewport<F>(
             return;
         }
 
-        ctx.push_view_box(vbox.0.width, vbox.0.height);
+        drawing_ctx::push_view_box(draw_ctx, vbox.0.width, vbox.0.height);
 
         let (x, y, w, h) =
             preserve_aspect_ratio.compute(vbox.0.width, vbox.0.height, vx, vy, vw, vh);
@@ -144,189 +69,20 @@ fn in_viewport<F>(
         affine.scale(w / vbox.0.width, h / vbox.0.height);
         affine.translate(-vbox.0.x, -vbox.0.y);
 
-        ctx.set_affine(affine);
+        drawing_ctx::get_cairo_context(draw_ctx).set_matrix(affine);
 
         if do_clip && clip_mode == ClipMode::ClipToVbox {
-            ctx.add_clipping_rect(vbox.0.x, vbox.0.y, vbox.0.width, vbox.0.height);
+            add_clipping_rect(draw_ctx, vbox.0.x, vbox.0.y, vbox.0.width, vbox.0.height);
         }
     } else {
-        ctx.push_view_box(vw, vh);
+        drawing_ctx::push_view_box(draw_ctx, vw, vh);
 
         affine.translate(vx, vy);
-        ctx.set_affine(affine);
+        drawing_ctx::get_cairo_context(draw_ctx).set_matrix(affine);
     }
 
     draw_fn();
 
-    ctx.pop_view_box();
-    ctx.pop_discrete_layer(node, values, clipping);
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    use std::ptr;
-    use std::rc::Rc;
-
-    use handle::RsvgHandle;
-    use node::{Node, NodeResult, NodeTrait, NodeType};
-    use parsers::Parse;
-    use property_bag::PropertyBag;
-
-    struct TestNodeImpl {}
-
-    impl NodeTrait for TestNodeImpl {
-        fn set_atts(&self, _: &RsvgNode, _: *const RsvgHandle, _: &PropertyBag) -> NodeResult {
-            Ok(())
-        }
-    }
-    #[derive(Default, PartialEq)]
-    struct Ctx {
-        pub view_box_size: Option<(f64, f64)>,
-        pub clipping_rect: Option<(f64, f64, f64, f64)>,
-        pub affine: Option<cairo::Matrix>,
-
-        pub expected_view_box_size: Option<(f64, f64)>,
-        pub expected_clipping_rect: Option<(f64, f64, f64, f64)>,
-        pub expected_affine: Option<cairo::Matrix>,
-    }
-
-    impl ViewportCtx for Ctx {
-        fn push_view_box(&mut self, width: f64, height: f64) {
-            self.view_box_size = Some((width, height));
-        }
-
-        fn pop_view_box(&mut self) {}
-
-        fn push_discrete_layer(&mut self, _values: &ComputedValues, _clipping: bool) {}
-
-        fn pop_discrete_layer(
-            &mut self,
-            _node: &RsvgNode,
-            _values: &ComputedValues,
-            _clipping: bool,
-        ) {
-        }
-
-        fn add_clipping_rect(&mut self, x: f64, y: f64, w: f64, h: f64) {
-            self.clipping_rect = Some((x, y, w, h));
-        }
-
-        fn set_affine(&mut self, affine: cairo::Matrix) {
-            self.affine = Some(affine);
-        }
-    }
-
-    fn call_in_viewport(
-        vx: f64,
-        vy: f64,
-        vw: f64,
-        vh: f64,
-        clip_mode: ClipMode,
-        do_clip: bool,
-        vbox: Option<ViewBox>,
-        preserve_aspect_ratio: AspectRatio,
-        affine: cairo::Matrix,
-        ctx: &mut Ctx,
-    ) {
-        let node = Rc::new(Node::new(
-            NodeType::Path,
-            None,
-            None,
-            None,
-            ptr::null_mut(),
-            Box::new(TestNodeImpl {}),
-        ));
-
-        in_viewport(
-            ctx,
-            vx,
-            vy,
-            vw,
-            vh,
-            clip_mode,
-            do_clip,
-            vbox,
-            preserve_aspect_ratio,
-            &node,
-            &ComputedValues::default(),
-            affine,
-            false,
-            || (),
-        );
-
-        assert_eq!(ctx.view_box_size, ctx.expected_view_box_size);
-        assert_eq!(ctx.clipping_rect, ctx.expected_clipping_rect);
-        assert_eq!(ctx.affine, ctx.expected_affine);
-    }
-
-    #[test]
-    fn clip_to_viewport() {
-        let mut affine = cairo::Matrix::identity();
-        affine.scale(0.25, 0.25);
-
-        let mut ctx = Ctx {
-            view_box_size: None,
-            clipping_rect: None,
-            affine: None,
-
-            expected_view_box_size: Some((40.0, 40.0)),
-            expected_clipping_rect: Some((10.0, 10.0, 10.0, 10.0)),
-            expected_affine: Some(affine),
-        };
-
-        call_in_viewport(
-            10.0,
-            10.0,
-            10.0,
-            10.0,
-            ClipMode::ClipToViewport,
-            true,
-            Some(ViewBox(cairo::Rectangle {
-                x: 40.0,
-                y: 40.0,
-                width: 40.0,
-                height: 40.0,
-            })),
-            AspectRatio::parse_str("xMidYMid meet", ()).unwrap(),
-            cairo::Matrix::identity(),
-            &mut ctx,
-        );
-    }
-
-    #[test]
-    fn clip_to_vbox() {
-        let mut affine = cairo::Matrix::identity();
-        affine.translate(10.0, 10.0);
-        affine.scale(0.40, 0.40);
-
-        let mut ctx = Ctx {
-            view_box_size: None,
-            clipping_rect: None,
-            affine: None,
-
-            expected_view_box_size: Some((50.0, 50.0)),
-            expected_clipping_rect: Some((0.0, 0.0, 50.0, 50.0)),
-            expected_affine: Some(affine),
-        };
-
-        call_in_viewport(
-            10.0,
-            10.0,
-            20.0,
-            20.0,
-            ClipMode::ClipToVbox,
-            true,
-            Some(ViewBox(cairo::Rectangle {
-                x: 0.0,
-                y: 0.0,
-                width: 50.0,
-                height: 50.0,
-            })),
-            AspectRatio::parse_str("xMidYMid meet", ()).unwrap(),
-            cairo::Matrix::identity(),
-            &mut ctx,
-        );
-    }
+    drawing_ctx::pop_view_box(draw_ctx);
+    drawing_ctx::pop_discrete_layer(draw_ctx, node, values, clipping);
 }
