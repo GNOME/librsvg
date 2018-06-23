d91448d0890fa9dfb59f52b80c4536f4c1ac6676
Jordan Petridis <jordanpetridis@protonmail.com>
2018-06-23 21:31:12 +0300

    Deny some extra rustc lints.

--- rsvg_internals/src/lib.rs
+++ rsvg_internals/src/lib.rs
@@ -1,6 +1,18 @@
+#![allow(unknown_lints)]
 #![cfg_attr(feature = "cargo-clippy", allow(clone_on_ref_ptr))]
 #![cfg_attr(feature = "cargo-clippy", allow(not_unsafe_ptr_arg_deref))]
 #![cfg_attr(feature = "cargo-clippy", allow(too_many_arguments))]
+// Enable lint group collections
+#![warn(nonstandard_style, edition_2018, rust_2018_idioms, bad_style, unused)]
+// standalone lints
+#![warn(
+    const_err, improper_ctypes, non_shorthand_field_patterns, no_mangle_generic_items,
+    overflowing_literals, plugin_as_library, private_no_mangle_fns, private_no_mangle_statics,
+    unconditional_recursion, unions_with_drop_fields, while_true, missing_debug_implementations,
+    trivial_casts, trivial_numeric_casts, elided_lifetime_in_paths, missing_copy_implementations
+)]
+// #![warn(missing_docs)]
+#![deny(warnings)]
 
 extern crate cairo;
 extern crate cairo_sys;
