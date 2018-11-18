9e92bdc28b0607d0226ed044973ca8524d9bf344
Julian Sparber <julian@sparber.net>
2018-11-18 12:13:31 +0200

    handle: add new API to get node geometry
    This adds a new API called rsvg_handle_get_geometry_sub(), it returns the
    exact position and dimension of a svg node.

--- tests/dimensions.c
+++ tests/dimensions.c
@@ -2,6 +2,7 @@
 /* vim: set ts=4 nowrap ai expandtab sw=4: */
 
 #include <glib.h>
+#include <cairo.h>
 #include "librsvg/rsvg.h"
 #include "test-utils.h"
 
@@ -24,6 +25,8 @@ test_dimensions (FixtureData *fixture)
     RsvgHandle *handle;
     RsvgPositionData position;
     RsvgDimensionData dimension;
+    cairo_rectangle_t ink_rect;
+    cairo_rectangle_t logical_rect;
     gchar *target_file;
     GError *error = NULL;
 
@@ -37,20 +40,29 @@ test_dimensions (FixtureData *fixture)
         g_assert (rsvg_handle_has_sub (handle, fixture->id));
         g_assert (rsvg_handle_get_position_sub (handle, &position, fixture->id));
         g_assert (rsvg_handle_get_dimensions_sub (handle, &dimension, fixture->id));
+        g_assert (rsvg_handle_get_geometry_sub (handle, &ink_rect, &logical_rect, fixture->id));
 
         g_message ("w=%d h=%d", dimension.width, dimension.height);
     } else {
         rsvg_handle_get_dimensions (handle, &dimension);
+        rsvg_handle_get_geometry_sub (handle, &ink_rect, &logical_rect, NULL);
     }
 
+    /* TODO: test logical position and dimension */
     if (fixture->has_position) {
         g_assert_cmpint (fixture->x, ==, position.x);
         g_assert_cmpint (fixture->y, ==, position.y);
+
+        g_assert_cmpint (fixture->x, ==, ink_rect.x);
+        g_assert_cmpint (fixture->y, ==, ink_rect.y);
     }
 
     if (fixture->has_dimensions) {
         g_assert_cmpint (fixture->width,  ==, dimension.width);
         g_assert_cmpint (fixture->height, ==, dimension.height);
+
+        g_assert_cmpint (fixture->width,  ==, ink_rect.width);
+        g_assert_cmpint (fixture->height, ==, ink_rect.height);
     }
 
     g_object_unref (handle);
