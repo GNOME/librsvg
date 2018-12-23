d85731109d2dcd5dc76f3d5460aecea1aaaf57a0
Kornel Lesiński <kornel@geekhood.net>
2018-12-23 14:32:27 +0000

    Don't use private enums submodule

--- rsvg_internals/src/drawing_ctx.rs
+++ rsvg_internals/src/drawing_ctx.rs
@@ -547,8 +547,8 @@ impl DrawingCtx {
             let mut options = cairo::FontOptions::new();
 
             options.set_antialias(cairo::Antialias::Gray);
-            options.set_hint_style(cairo::enums::HintStyle::Full);
-            options.set_hint_metrics(cairo::enums::HintMetrics::On);
+            options.set_hint_style(cairo::HintStyle::Full);
+            options.set_hint_metrics(cairo::HintMetrics::On);
 
             pangocairo::functions::context_set_font_options(&context, &options);
         }
