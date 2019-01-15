6c104477ec9cf90194b1a675f104fa97b82ec368
Paolo Borelli <pborelli@gnome.org>
2019-01-15 12:37:13 +0100

    svg: store image resources in the svg struct
    This makes it consistent with xml resources. It also allows images
    to be cached in case they are rendered more than once.
    The images are loaded lazily during rendering which goes in the
    opposite direction with the goal of having an immutable Svg
    object, but at least this is the same of other resources. Beside
    images are just used in two places, and filters/images was
    already doing a hack to load the image at rendering time.

--- rsvg_internals/src/svg.rs
+++ rsvg_internals/src/svg.rs
@@ -1,4 +1,7 @@
+use cairo::{ImageSurface, Status};
+use gdk_pixbuf::{PixbufLoader, PixbufLoaderExt};
 use gio;
+use glib::translate::*;
 use std::cell::RefCell;
 use std::collections::hash_map::Entry;
 use std::collections::HashMap;
@@ -10,8 +13,10 @@ use handle::LoadOptions;
 use io;
 use node::RsvgNode;
 use state::ComputedValues;
+use surface_utils::shared_surface::SharedImageSurface;
 use xml::XmlState;
 use xml2_load::xml_state_load_from_possibly_compressed_stream;
+
 
 /// A loaded SVG file and its derived data
 ///
@@ -22,10 +27,11 @@ pub struct Svg {
 
     ids: HashMap<String, RsvgNode>,
 
-    // This requires interior mutability because we load the extern
+    // These require interior mutability because we load the extern
     // resources all over the place.  Eventually we'll be able to do this
     // once, at loading time, and keep this immutable.
     externs: RefCell<Resources>,
+    images: RefCell<Images>,
 
     // Once we do not need to load externs, we can drop this as well
     load_options: LoadOptions,
@@ -40,6 +46,7 @@ impl Svg {
             tree,
             ids,
             externs: RefCell::new(Resources::new()),
+            images: RefCell::new(Images::new()),
             load_options,
         }
     }
@@ -73,6 +80,13 @@ impl Svg {
 
     pub fn lookup_node_by_id(&self, id: &str) -> Option<RsvgNode> {
         self.ids.get(id).map(|n| (*n).clone())
+    }
+
+    pub fn lookup_image(&self, href: &str) -> Option<ImageSurface> {
+        self.images
+            .borrow_mut()
+            .lookup(&self.load_options, href)
+            .and_then(|s| s.into_image_surface().ok())
     }
 }
 
@@ -117,9 +131,93 @@ impl Resources {
     }
 }
 
+struct Images {
+    images: HashMap<AllowedUrl, SharedImageSurface>,
+}
+
+impl Images {
+    pub fn new() -> Images {
+        Images {
+            images: Default::default(),
+        }
+    }
+
+    /// Returns a node referenced by a fragment ID, from an
+    /// externally-loaded SVG file.
+    pub fn lookup(&mut self, load_options: &LoadOptions, href: &str) -> Option<SharedImageSurface> {
+        // FIXME: propagate errors
+        let aurl = AllowedUrl::from_href(href, load_options.base_url.as_ref()).ok()?;
+
+        match self.images.entry(aurl) {
+            Entry::Occupied(e) => Some(e.get().clone()),
+            Entry::Vacant(e) => {
+                // FIXME: propagate errors
+                let surface = load_image(load_options, e.key()).ok()?;
+                let res = e.insert(surface);
+                Some(res.clone())
+            }
+        }
+    }
+}
+
 fn load_svg(load_options: &LoadOptions, aurl: &AllowedUrl) -> Result<Svg, LoadingError> {
     // FIXME: pass a cancellable to these
     io::acquire_stream(aurl, None).and_then(|stream| {
         Svg::load_from_stream(&load_options.copy_with_base_url(aurl), &stream, None)
     })
+}
+
+fn load_image(load_options: &LoadOptions, aurl: &AllowedUrl) -> Result<SharedImageSurface, LoadingError> {
+    let data = io::acquire_data(&aurl, None)?;
+
+    if data.data.len() == 0 {
+        return Err(LoadingError::EmptyData);
+    }
+
+    let loader = if let Some(ref content_type) = data.content_type {
+        PixbufLoader::new_with_mime_type(content_type)?
+    } else {
+        PixbufLoader::new()
+    };
+
+    loader.write(&data.data)?;
+    loader.close()?;
+
+    let pixbuf = loader.get_pixbuf().ok_or(LoadingError::Unknown)?;
+
+    let surface = SharedImageSurface::from_pixbuf(&pixbuf)?;
+
+    if load_options.flags.keep_image_data {
+        if let Some(mime_type) = data.content_type {
+            extern "C" {
+                fn cairo_surface_set_mime_data(
+                    surface: *mut cairo_sys::cairo_surface_t,
+                    mime_type: *const libc::c_char,
+                    data: *mut libc::c_char,
+                    length: libc::c_ulong,
+                    destroy: cairo_sys::cairo_destroy_func_t,
+                    closure: *mut libc::c_void,
+                ) -> Status;
+            }
+
+            let data_ptr = ToGlibContainerFromSlice::to_glib_full_from_slice(&data.data);
+
+            unsafe {
+                let status = cairo_surface_set_mime_data(
+                    surface.to_glib_none().0,
+                    mime_type.to_glib_none().0,
+                    data_ptr as *mut _,
+                    data.data.len() as libc::c_ulong,
+                    Some(glib_sys::g_free),
+                    data_ptr as *mut _,
+                );
+
+                if status != Status::Success {
+                    return Err(LoadingError::Cairo(status));
+                }
+            }
+        }
+    }
+
+    Ok(surface)
 }
