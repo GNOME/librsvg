2b7578af1a0e05f283dffbb139f43e3ea7a314dd
Federico Mena Quintero <federico@gnome.org>
2019-01-25 12:34:02 -0600

    Add docs for librsvg_version and associated variables

--- librsvg/librsvg-features.c
+++ librsvg/librsvg-features.c
@@ -1,7 +1,32 @@
 #include "rsvg.h"
 
-/* General initialization hooks */
+/**
+ * librsvg_major_version:
+ *
+ * Major version of the library.  For example, for version 2.3.4, the major
+ * version will be 2.
+ */
 const guint librsvg_major_version = LIBRSVG_MAJOR_VERSION;
+
+/**
+ * librsvg_minor_version:
+ *
+ * Minor version of the library.  For example, for version 2.3.4, the minor
+ * version will be 3.
+ */
 const guint librsvg_minor_version = LIBRSVG_MINOR_VERSION;
+
+/**
+ * librsvg_micro_version:
+ *
+ * Micro version of the library.  For example, for version 2.3.4, the micro
+ * version will be 4.
+ */
 const guint librsvg_micro_version = LIBRSVG_MICRO_VERSION;
+
+/**
+ * librsvg_version:
+ *
+ * String with the library version.  For example, "<literal>2.3.4</literal>".
+ */
 const char librsvg_version[] = LIBRSVG_VERSION;
