413e987738604a1e53839f258efb0eabcc848fc2
Kornel Lesiński <kornel@geekhood.net>
2019-02-12 13:02:42 +0000

    Replicate librsvg-features.h in Cargo

--- librsvg/build.rs
+++ librsvg/build.rs
@@ -8,6 +8,12 @@ fn main() {
     // Expose internals
     cc.define("RSVG_COMPILATION", Some("1"));
 
+    cc.define("GETTEXT_PACKAGE", Some("\"librsvg\""));
+    cc.define("LIBRSVG_MAJOR_VERSION", Some(env::var("CARGO_PKG_VERSION_MAJOR").unwrap().as_str()));
+    cc.define("LIBRSVG_MINOR_VERSION", Some(env::var("CARGO_PKG_VERSION_MINOR").unwrap().as_str()));
+    cc.define("LIBRSVG_MICRO_VERSION", Some(env::var("CARGO_PKG_VERSION_PATCH").unwrap().as_str()));
+    cc.define("LIBRSVG_VERSION", Some(format!("\"{}\"", env::var("CARGO_PKG_VERSION").unwrap()).as_str()));
+
     for var_name in ["DEP_GLIB_INCLUDE", "DEP_CAIRO_INCLUDE", "DEP_PANGO_INCLUDE", "DEP_GDK_PIXBUF_INCLUDE"].iter() {
         let var = env::var_os(var_name).expect(var_name);
         for inc in env::split_paths(&var) {
