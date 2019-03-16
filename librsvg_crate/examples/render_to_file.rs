16a78f2419bf73af3a436b2b1b86bc8407327a25
Jordan Petridis <jpetridis@gnome.org>
2019-03-16 17:47:34 +0200

    (446): Accept IsA<InputStream> instead of &InputStream wherever possible
    This should allow for a more flexible api and avoid the need of
    casting before using it.
    
    Close #446

---
+++ librsvg_crate/examples/render_to_file.rs
@@ -0,0 +1,32 @@
+extern crate cairo;
+extern crate gio;
+extern crate glib;
+extern crate librsvg;
+
+fn main() {
+    let width = 96.0;
+    let height = 96.0;
+    let output = "output.svg";
+
+    let bytes = glib::Bytes::from_static(include_bytes!("../org.gnome.Epiphany.svg"));
+    let stream = gio::MemoryInputStream::new_from_bytes(&bytes);
+    let handle = librsvg::Loader::new()
+        .read_stream(stream, None, None)
+        .unwrap();
+    let renderer = librsvg::CairoRenderer::new(&handle);
+
+    let surface = cairo::svg::File::new(width, height, output);
+    let cr = cairo::Context::new(&surface);
+    renderer
+        .render_element_to_viewport(
+            &cr,
+            None,
+            &cairo::Rectangle {
+                x: 0.0,
+                y: 0.0,
+                width,
+                height,
+            },
+        )
+        .unwrap();
+}
