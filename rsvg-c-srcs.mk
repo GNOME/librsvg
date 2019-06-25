81a4e323f21b8ad21445f054d95ae346cbeaf5a5
Chun-wei Fan <fanchunwei@src.gnome.org>
2019-06-25 17:37:43 +0800

    Makefile.am: Split out the C sources list
    This will prepare for us to be able to share the source listings with
    other Makefile-based build systems, which will pave the way to build
    librsvg with NMake all-the-way, and will replace the Visual Studio
    projects.

---
+++ rsvg-c-srcs.mk
@@ -0,0 +1,20 @@
+headers =			\
+	librsvg/rsvg.h		\
+	librsvg/rsvg-cairo.h
+
+extra_inc_headers =	\
+	librsvg/librsvg-features.h
+
+librsvg_c_srcs =	\
+	librsvg/librsvg-features.c 		\
+	librsvg/librsvg-features.h 		\
+	librsvg/rsvg-base.c			\
+	librsvg/rsvg-cairo.h			\
+	librsvg/rsvg-css.h 			\
+	librsvg/rsvg-handle.c			\
+	librsvg/rsvg-pixbuf.c			\
+	librsvg/rsvg.h				\
+	$(NULL)
+
+rsvg_convert_srcs = rsvg-convert.c
+rsvg_view_srcs = rsvg-view.c
