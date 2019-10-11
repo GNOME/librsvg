2151b3b9bff277543b39a77054b9f0291b42a790
Federico Mena Quintero <federico@gnome.org>
2019-10-10 19:28:26 -0500

    Add test for the billion-laughs equivalent from issue #515
    This is billion-laughs, but done with elements inside <pattern>.  They
    in turn reference a fill="url(#next_sublevel_pattern)".

--- tests/errors.c
+++ tests/errors.c
@@ -40,9 +40,10 @@ test_non_svg_element (void)
 }
 
 static void
-test_instancing_limit (void)
+test_instancing_limit (gconstpointer data)
 {
-    char *filename = get_test_filename ("323-nested-use.svg");
+    const char *basename = data;
+    char *filename = get_test_filename (basename);
     RsvgHandle *handle;
     GError *error = NULL;
     cairo_surface_t *surf;
@@ -67,7 +68,17 @@ main (int argc, char **argv)
     g_test_init (&argc, &argv, NULL);
 
     g_test_add_func ("/errors/non_svg_element", test_non_svg_element);
-    g_test_add_func ("/errors/instancing_limit", test_instancing_limit);
+
+    g_test_add_data_func_full ("/errors/instancing_limit/323-nested-use.svg",
+                               "323-nested-use.svg",
+                               test_instancing_limit,
+                               NULL);
+
+    g_test_add_data_func_full ("/errors/instancing_limit/515-pattern-billion-laughs.svg",
+                               "515-pattern-billion-laughs.svg",
+                               test_instancing_limit,
+                               NULL);
+
 
     return g_test_run ();
 }
