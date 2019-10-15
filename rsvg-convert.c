bd8ebc43e5325571fa71de82cb1b38ee30caf024
Federico Mena Quintero <federico@gnome.org>
2019-10-15 16:47:30 -0500

    Use the LGPL 2.1 consistently
    Fixes these:
    https://gitlab.gnome.org/GNOME/librsvg/issues/72
    https://gitlab.gnome.org/GNOME/librsvg/issues/432

--- rsvg-convert.c
+++ rsvg-convert.c
@@ -7,25 +7,26 @@
    Copyright (C) 2005 Red Hat, Inc.
    Copyright (C) 2005 Dom Lachowicz <cinamod@hotmail.com>
    Copyright (C) 2005 Caleb Moore <c.moore@student.unsw.edu.au>
+   Copyright (C) 2019 Federico Mena Quintero <federico@gnome.org>
   
-   This program is free software; you can redistribute it and/or
-   modify it under the terms of the GNU Library General Public License as
-   published by the Free Software Foundation; either version 2 of the
-   License, or (at your option) any later version.
-  
-   This program is distributed in the hope that it will be useful,
+   This library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Lesser General Public
+   License as published by the Free Software Foundation; either
+   version 2.1 of the License, or (at your option) any later version.
+
+   This library is distributed in the hope that it will be useful,
    but WITHOUT ANY WARRANTY; without even the implied warranty of
    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-   Library General Public License for more details.
-  
-   You should have received a copy of the GNU Library General Public
-   License along with this program; if not, write to the
-   Free Software Foundation, Inc., 59 Temple Place - Suite 330,
-   Boston, MA 02111-1307, USA.
+   Lesser General Public License for more details.
+
+   You should have received a copy of the GNU Lesser General Public
+   License along with this library; if not, write to the Free Software
+   Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
   
    Authors: Carl Worth <cworth@cworth.org>, 
             Caleb Moore <c.moore@student.unsw.edu.au>,
-            Dom Lachowicz <cinamod@hotmail.com>
+            Dom Lachowicz <cinamod@hotmail.com>,
+            Federico Mena Quintero <federico@gnome.org>
 */
 
 #define RSVG_DISABLE_DEPRECATION_WARNINGS
