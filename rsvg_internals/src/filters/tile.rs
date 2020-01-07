50c99346195e8c847d4e57ed6e7ce5278417007d
Paolo Borelli <pborelli@gnome.org>
2020-01-07 19:50:19 +0100

    tile: move cairo code out of the tile filter
    Factor out a tile and paint_repeated_image method on SharedImageSurface

--- rsvg_internals/src/filters/tile.rs
+++ rsvg_internals/src/filters/tile.rs
@@ -1,7 +1,6 @@
 use crate::drawing_ctx::DrawingCtx;
 use crate::node::{NodeResult, NodeTrait, RsvgNode};
 use crate::property_bag::PropertyBag;
-use crate::surface_utils::shared_surface::SharedImageSurface;
 
 use super::context::{FilterContext, FilterInput, FilterOutput, FilterResult};
 use super::{FilterEffect, FilterError, PrimitiveWithInput};
@@ -41,62 +40,26 @@ impl FilterEffect for FeTile {
         // feTile doesn't consider its inputs in the filter primitive subregion calculation.
         let bounds = self.base.get_bounds(ctx).into_irect(draw_ctx);
 
-        let output_surface = match input {
-            FilterInput::StandardInput(surface) => surface,
+        let surface = match input {
+            FilterInput::StandardInput(input_surface) => input_surface,
             FilterInput::PrimitiveOutput(FilterOutput {
-                surface,
+                surface: input_surface,
                 bounds: input_bounds,
             }) => {
-                // Create a surface containing just the region to tile.
-                let bounded_input_surface = cairo::ImageSurface::create(
-                    cairo::Format::ARgb32,
-                    input_bounds.width(),
-                    input_bounds.height(),
-                )?;
+                let tile_surface = input_surface.tile(input_bounds)?;
 
-                {
-                    let cr = cairo::Context::new(&bounded_input_surface);
-                    surface.set_as_source_surface(
-                        &cr,
-                        f64::from(-input_bounds.x0),
-                        f64::from(-input_bounds.y0),
-                    );
-                    cr.paint();
-                }
-
-                // Make a pattern out of the tile region.
-                let ptn = cairo::SurfacePattern::create(&bounded_input_surface);
-                ptn.set_extend(cairo::Extend::Repeat);
-                let mut mat = cairo::Matrix::identity();
-                mat.translate(f64::from(-input_bounds.x0), f64::from(-input_bounds.y0));
-                ptn.set_matrix(mat);
-
-                let output_surface = cairo::ImageSurface::create(
-                    cairo::Format::ARgb32,
-                    ctx.source_graphic().width(),
-                    ctx.source_graphic().height(),
-                )?;
-
-                {
-                    let cr = cairo::Context::new(&output_surface);
-                    let r = cairo::Rectangle::from(bounds);
-                    cr.rectangle(r.x, r.y, r.width, r.height);
-                    cr.clip();
-
-                    cr.set_source(&ptn);
-                    cr.paint();
-                }
-
-                SharedImageSurface::new(output_surface, surface.surface_type())?
+                ctx.source_graphic().paint_image_tiled(
+                    bounds,
+                    &tile_surface,
+                    -input_bounds.x0,
+                    -input_bounds.y0,
+                )?
             }
         };
 
         Ok(FilterResult {
             name: self.base.result.clone(),
-            output: FilterOutput {
-                surface: output_surface,
-                bounds,
-            },
+            output: FilterOutput { surface, bounds },
         })
     }
 
