b3cfd1cc0aa5e680dbf5202b3fc026f8f67e1d03
Federico Mena Quintero <federico@gnome.org>
2020-05-28 18:13:37 -0500

    Use the rows iterator in pixbuf_from_surface to iterate the SharedImageSurface
    The next step is to iterate quickly on the pixbuf.

--- rsvg_internals/src/lib.rs
+++ rsvg_internals/src/lib.rs
@@ -58,6 +58,7 @@ pub use crate::structure::IntrinsicDimensions;
 pub use crate::surface_utils::{
     iterators::Pixels,
     shared_surface::{SharedImageSurface, SurfaceType},
+    Pixel,
 };
 
 pub use crate::viewbox::ViewBox;
