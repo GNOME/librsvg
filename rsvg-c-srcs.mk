deb98df08ad0a5b760f6d40f3c52f72a98d45b4c
Sven Neumann <sven@svenfoo.org>
2020-09-29 13:32:17 +0200

    Revert some more changes to the build system
    These are part of commit b8c8756a0e8148afb2752acb62f06c57c917e4b4
    and need to be revisited more carefully as they affect the GObject
    introspection and Vala bindings.

--- rsvg-c-srcs.mk
+++ rsvg-c-srcs.mk
@@ -3,15 +3,17 @@ headers =			\
 	librsvg/rsvg-cairo.h	\
 	librsvg/rsvg-features.h
 
-extra_inc_headers =		\
+extra_inc_headers =	\
 	librsvg/rsvg-version.h
 
-librsvg_c_srcs =		\
-	librsvg/rsvg-cairo.h	\
-	librsvg/rsvg-css.h 	\
-	librsvg/rsvg-features.h \
-	librsvg/rsvg-version.h	\
-	librsvg/rsvg.h		\
+librsvg_c_srcs =	\
+	librsvg/rsvg-base.c		\
+	librsvg/rsvg-cairo.h		\
+	librsvg/rsvg-css.h 		\
+	librsvg/rsvg-features.h 	\
+	librsvg/rsvg-handle.c		\
+	librsvg/rsvg-pixbuf.c		\
+	librsvg/rsvg.h			\
 	$(NULL)
 
 rsvg_convert_srcs = rsvg-convert.c
