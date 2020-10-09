020b79c83ba23749019aa946fe1001d897b65d01
Federico Mena Quintero <federico@gnome.org>
2020-10-09 17:14:25 -0500

    Move the tests for the legacy API for loading to api.c

--- tests/api.c
+++ tests/api.c
@@ -1524,6 +1524,63 @@ static DimensionsFixtureData dimensions_fixtures[] =
     },
 };
 
+typedef struct
+{
+    const char *test_name;
+    const char *fixture;
+    size_t buf_size;
+} LoadingTestData;
+
+static void
+load_n_bytes_at_a_time (gconstpointer data)
+{
+    const LoadingTestData *fixture_data = data;
+    char *filename = g_build_filename (test_utils_get_test_data_path (), fixture_data->fixture, NULL);
+    guchar *buf = g_new (guchar, fixture_data->buf_size);
+    gboolean done;
+
+    RsvgHandle *handle;
+    FILE *file;
+
+    file = fopen (filename, "rb");
+    g_assert_nonnull (file);
+
+    handle = rsvg_handle_new_with_flags (RSVG_HANDLE_FLAGS_NONE);
+
+    done = FALSE;
+
+    do {
+        size_t num_read;
+
+        num_read = fread (buf, 1, fixture_data->buf_size, file);
+
+        if (num_read > 0) {
+            g_assert_true (rsvg_handle_write (handle, buf, num_read, NULL));
+        } else {
+            g_assert_cmpint (ferror (file), ==, 0);
+
+            if (feof (file)) {
+                done = TRUE;
+            }
+        }
+    } while (!done);
+
+    fclose (file);
+    g_free (filename);
+
+    g_assert_true (rsvg_handle_close (handle, NULL));
+
+    g_object_unref (handle);
+
+    g_free (buf);
+}
+
+static LoadingTestData loading_tests[] = {
+    { "/loading/one-byte-at-a-time", "loading/gnome-cool.svg", 1 },
+    { "/loading/compressed-one-byte-at-a-time", "loading/gnome-cool.svgz", 1 },
+    { "/loading/compressed-two-bytes-at-a-time", "loading/gnome-cool.svgz", 2 } /* to test reading the entire gzip header */
+};
+
 /* Tests for the deprecated GdkPixbuf-based API */
 static void
 add_pixbuf_tests (void)
@@ -1596,6 +1653,17 @@ add_geometry_tests (void)
         g_test_add_data_func (dimensions_fixtures[i].test_name, &dimensions_fixtures[i], (void*)test_dimensions);
 }
 
+/* Tests for the deprecated API for loading bytes at a time */
+static void
+add_loading_tests (void)
+{
+    int i;
+
+    for (i = 0; i < G_N_ELEMENTS (loading_tests); i++) {
+        g_test_add_data_func (loading_tests[i].test_name, &loading_tests[i], load_n_bytes_at_a_time);
+    }
+}
+
 int
 main (int argc, char **argv)
 {
@@ -1606,6 +1674,7 @@ main (int argc, char **argv)
     add_pixbuf_tests ();
     add_api_tests ();
     add_geometry_tests ();
+    add_loading_tests ();
 
     return g_test_run ();
 }
