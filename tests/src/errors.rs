df58a906e021feb371dd15be01becd81e8ab1782
Federico Mena Quintero <federico@gnome.org>
2020-10-13 19:35:19 -0500

    Move the rest of the errors tests to Rust

--- tests/src/errors.rs
+++ tests/src/errors.rs
@@ -3,7 +3,8 @@
 #![cfg(test)]
 use test_generator::test_resources;
 
-use librsvg::{Loader, LoadingError};
+use cairo;
+use librsvg::{CairoRenderer, Loader, LoadingError, RenderingError};
 
 use crate::utils::fixture_path;
 
@@ -13,4 +14,39 @@ fn loading_crash(name: &str) {
         Loader::new().read_path(fixture_path(name)),
         Err(LoadingError::XmlParseError(_))
     ));
+}
+
+fn rendering_instancing_limit(name: &str) {
+    let handle = Loader::new()
+        .read_path(fixture_path(name))
+        .unwrap_or_else(|e| panic!("could not load: {}", e));
+
+    let surface = cairo::ImageSurface::create(cairo::Format::ARgb32, 500, 500).unwrap();
+    let cr = cairo::Context::new(&surface);
+
+    // Note that at least 515-patttern-billion-laughs.svg requires a viewport of this size
+    // or bigger; a smaller one causes the recursive patterns to get so small that they
+    // are culled out, and so the document doesn't reach the instancing limit.
+    assert!(matches!(
+        CairoRenderer::new(&handle).render_document(
+            &cr,
+            &cairo::Rectangle {
+                x: 0.0,
+                y: 0.0,
+                width: 500.0,
+                height: 500.0,
+            },
+        ),
+        Err(RenderingError::InstancingLimit)
+    ));
+}
+
+#[test_resources("tests/fixtures/errors/323-nested-use.svg")]
+fn instancing_limit1(name: &str) {
+    rendering_instancing_limit(name);
+}
+
+#[test_resources("tests/fixtures/errors/515-pattern-billion-laughs.svg")]
+fn instancing_limit2(name: &str) {
+    rendering_instancing_limit(name);
 }
