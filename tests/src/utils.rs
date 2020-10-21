17fe8df2b4952f6826ccb316a6de510345ae8d4d
Sven Neumann <sven@svenfoo.org>
2020-10-21 17:08:19 +0200

    Add code to create a FontConfig

--- tests/src/utils.rs
+++ tests/src/utils.rs
@@ -3,6 +3,8 @@
 use std::env;
 use std::path::PathBuf;
 
+#[cfg(have_pangoft2)]
+
 /// Given a filename from `test_generator::test_resources`, computes the correct fixture filename.
 ///
 /// The `test_resources` procedural macro works by running a filename glob starting on
@@ -22,12 +24,36 @@ pub fn fixture_path(filename_from_test_resources: &str) -> PathBuf {
 
 #[cfg(have_pangoft2)]
 pub fn setup_font_map() {
+    use fontconfig_sys::fontconfig;
     use glib::prelude::*;
     use pangocairo::FontMap;
 
+    let font_paths = [
+        "tests/resources/Roboto-Regular.ttf",
+        "tests/resources/Roboto-Italic.ttf",
+        "tests/resources/Roboto-Bold.ttf",
+        "tests/resources/Roboto-BoldItalic.ttf",
+    ];
+
+    let config = unsafe { fontconfig::FcConfigCreate() };
+
+    for path in &font_paths {
+        let path = fixture_path(path);
+        let str = path.to_str().unwrap();
+
+        unsafe {
+            if fontconfig::FcConfigAppFontAddFile(config, str.as_ptr()) == 0 {
+                panic!("Could not load font file {} for tests; aborting", str);
+            }
+        };
+    }
+
     let font_map = FontMap::new_for_font_type(cairo::FontType::FontTypeFt);
 
-    // TODO: create and apply FontConfig
+    // TODO: apply config
+    unsafe {
+        fontconfig::FcConfigDestroy(config);
+    };
 
     FontMap::set_default(font_map.map(|m| m.downcast::<pangocairo::FontMap>().unwrap()));
 }
