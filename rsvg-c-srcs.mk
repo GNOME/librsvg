482c809d084bc2b1c1f782beb658e2e5d7296cc4
Federico Mena Quintero <federico@gnome.org>
2020-10-30 13:02:23 -0600

    Start fixing autotools
    Rename librsvg/ to include/ since now it only has header files.

--- rsvg-c-srcs.mk
+++ rsvg-c-srcs.mk
@@ -1,12 +1,12 @@
 librsvg_public_headers =	\
-	librsvg/rsvg.h		\
-	librsvg/rsvg-cairo.h	\
-	librsvg/rsvg-features.h \
-	librsvg/rsvg-version.h	\
+	include/rsvg.h		\
+	include/rsvg-cairo.h	\
+	include/rsvg-features.h \
+	include/rsvg-version.h	\
 	$(NULL)
 
 librsvg_private_headers = 	\
-	librsvg/rsvg-css.h	\
+	include/rsvg-css.h	\
 	$(NULL)
 
 rsvg_convert_srcs = rsvg-convert.c
