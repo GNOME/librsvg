7c59374baca2e572fc2661ce1cad0d7d4f96727a
madds-h <madeline@hollandart.io>
2021-04-25 18:10:24 -0500

    Remove support for NumberList::Unbounded

--- src/parsers.rs
+++ src/parsers.rs
@@ -135,7 +135,6 @@ impl Parse for u32 {
 pub enum NumberListLength {
     MaxLength(usize),
     Exact(usize),
-    Unbounded,
 }
 
 #[derive(Debug, PartialEq)]
@@ -155,12 +154,7 @@ impl NumberList {
             }
             NumberListLength::Exact(l) if l > 0 => Vec::<f64>::with_capacity(l),
             NumberListLength::Exact(_) => unreachable!("NumberListLength::Exact cannot be 0"),
-            NumberListLength::Unbounded => Vec::<f64>::new(),
         };
-
-        if parser.is_exhausted() && length == NumberListLength::Unbounded {
-            return Ok(NumberList(v));
-        }
 
         for i in 0.. {
             if i != 0 {
@@ -388,22 +382,13 @@ mod tests {
             NumberList::parse_str("1 2 3 4", NumberListLength::Exact(4)).unwrap(),
             NumberList(vec![1.0, 2.0, 3.0, 4.0])
         );
-
-        assert_eq!(
-            NumberList::parse_str("", NumberListLength::Unbounded).unwrap(),
-            NumberList(vec![])
-        );
-
-        assert_eq!(
-            NumberList::parse_str("1, 2, 3.0, 4, 5", NumberListLength::Unbounded).unwrap(),
-            NumberList(vec![1.0, 2.0, 3.0, 4.0, 5.0])
-        );
     }
 
     #[test]
     fn errors_on_invalid_number_list() {
         // empty
         assert!(NumberList::parse_str("", NumberListLength::Exact(1)).is_err());
+        assert!(NumberList::parse_str("", NumberListLength::MaxLength(1)).is_err());
 
         // garbage
         assert!(NumberList::parse_str("foo", NumberListLength::Exact(1)).is_err());
@@ -418,7 +403,6 @@ mod tests {
         // extra token
         assert!(NumberList::parse_str("1,", NumberListLength::Exact(1)).is_err());
         assert!(NumberList::parse_str("1,", NumberListLength::Exact(1)).is_err());
-        assert!(NumberList::parse_str("1,", NumberListLength::Unbounded).is_err());
 
         // too few
         assert!(NumberList::parse_str("1", NumberListLength::Exact(2)).is_err());
