b799710f51363e89291ba81e925c1c217036e1c1
Federico Mena Quintero <federico@gnome.org>
2021-09-30 17:10:19 -0500

    Test for no intrinsic size at all
    Part-of: <https://gitlab.gnome.org/GNOME/librsvg/-/merge_requests/595>

--- tests/src/legacy_sizing.rs
+++ tests/src/legacy_sizing.rs
@@ -29,3 +29,31 @@ fn just_viewbox_uses_viewbox_size() {
          })
     );
 }
+
+#[test]
+fn no_intrinsic_size_uses_element_geometries() {
+    let svg = load_svg(
+        br#"<?xml version="1.0" encoding="UTF-8"?>
+<svg xmlns="http://www.w3.org/2000/svg">
+  <rect x="10" y="20" width="30" height="40" fill="black"/>
+</svg>
+"#,
+    )
+        .unwrap();
+
+    assert_eq!(
+        CairoRenderer::new(&svg).legacy_layer_geometry(None).unwrap(),
+        (cairo::Rectangle {
+            x: 10.0,
+            y: 20.0,
+            width: 30.0,
+            height: 40.0,
+        },
+         cairo::Rectangle {
+             x: 10.0,
+             y: 20.0,
+             width: 30.0,
+             height: 40.0,
+         })
+    );
+}
