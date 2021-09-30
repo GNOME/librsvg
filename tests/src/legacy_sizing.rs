3ce1b78c5d2941b43870132dcc359a2a1d448ec9
Federico Mena Quintero <federico@gnome.org>
2021-09-30 17:13:26 -0500

    Test for 100% width and height
    Part-of: <https://gitlab.gnome.org/GNOME/librsvg/-/merge_requests/595>

--- tests/src/legacy_sizing.rs
+++ tests/src/legacy_sizing.rs
@@ -57,3 +57,29 @@ fn no_intrinsic_size_uses_element_geometries() {
          })
     );
 }
+
+#[test]
+fn hundred_percent_width_height_uses_viewbox() {
+    let svg = load_svg(
+        br#"<?xml version="1.0" encoding="UTF-8"?>
+<svg xmlns="http://www.w3.org/2000/svg" width="100%" height="100%" viewBox="0 0 100 200"/>
+"#,
+    )
+        .unwrap();
+
+    assert_eq!(
+        CairoRenderer::new(&svg).legacy_layer_geometry(None).unwrap(),
+        (cairo::Rectangle {
+            x: 0.0,
+            y: 0.0,
+            width: 100.0,
+            height: 200.0,
+        },
+         cairo::Rectangle {
+             x: 0.0,
+             y: 0.0,
+             width: 100.0,
+             height: 200.0,
+         })
+    );
+}
