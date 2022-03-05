52a98a03402812468b99ec4cc959d66e4deda553
Federico Mena Quintero <federico@gnome.org>
2022-03-04 20:15:24 -0600

    Add tests for when ellipse's rx/ry properties default to "auto"
    Yay, code coverage!
    
    Part-of: <https://gitlab.gnome.org/GNOME/librsvg/-/merge_requests/670>

---
+++ tests/src/shapes.rs
@@ -0,0 +1,13 @@
+use crate::test_svg_reference;
+
+test_svg_reference!(
+    ellipse_auto_rx_ry,
+    "tests/fixtures/reftests/svg2-reftests/ellipse-auto-rx-ry.svg",
+    "tests/fixtures/reftests/svg2-reftests/ellipse-auto-rx-ry-ref.svg"
+);
+
+test_svg_reference!(
+    ellipse_single_auto_rx_ry,
+    "tests/fixtures/reftests/svg2-reftests/ellipse-single-auto-rx-ry.svg",
+    "tests/fixtures/reftests/svg2-reftests/ellipse-single-auto-rx-ry-ref.svg"
+);
