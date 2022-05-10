GNOME/librsvg

commit e3bb911b2cd860e4e4671c87ea9712913bb35d26

DrawingCtx.nested() - replaces push_cairo_context / pop_cairo_context
The cr_stack is just used to keep a stack of surfaces for compositing
and their transforms (for compositing in get_snapshot(), used by the
filters code).

In reality, push_cairo_context / pop_cairo_context were used as a way
to paint to another cr, that comes from another surface.

Now, we can create a "nested" DrawingCtx with respect to the other cr.
Maybe this will let us unify DrawingCtx / surface / cr creation.

src/drawing_ctx.rs
@@ -243,6 +243,12 @@ impl Drop for SavedCr<'_> {
     }
 }
 
+impl Drop for DrawingCtx {
+    fn drop(&mut self) {
+        self.cr_stack.borrow_mut().pop();
+    }
+}
+
 impl DrawingCtx {
     fn new(
         cr: &cairo::Context,
@@ -267,6 +273,23 @@ impl DrawingCtx {
             drawsub_stack,
             measuring,
             testing,
+        }
+    }
+
+    fn nested(&self, cr: cairo::Context) -> DrawingCtx {
+        let cr_stack = self.cr_stack.clone();
+
+        cr_stack.borrow_mut().push(self.cr.clone());
+
+        DrawingCtx {
+            initial_viewport: self.initial_viewport,
+            dpi: self.dpi,
+            cr_stack,
+            cr,
+            viewport_stack: self.viewport_stack.clone(),
+            drawsub_stack: Vec::new(),
+            measuring: self.measuring,
+            testing: self.testing,
         }
     }
 
@@ -300,17 +323,6 @@ impl DrawingCtx {
         let res = draw_fn(self);
         self.cr = cr_save;
         res
-    }
-
-    // Temporary hack while we unify surface/cr/affine creation
-    fn push_cairo_context(&mut self, cr: cairo::Context) {
-        self.cr_stack.borrow_mut().push(self.cr.clone());
-        self.cr = cr;
-    }
-
-    // Temporary hack while we unify surface/cr/affine creation
-    fn pop_cairo_context(&mut self) {
-        self.cr = self.cr_stack.borrow_mut().pop().unwrap();
     }
 
     fn size_for_temporary_surface(&self) -> (i32, i32) {
@@ -565,17 +577,15 @@ impl DrawingCtx {
 
             let _params = self.push_coord_units(mask.get_content_units());
 
-            self.push_cairo_context(mask_cr);
+            let mut mask_draw_ctx = self.nested(mask_cr);
 
-            let res = self.with_discrete_layer(
+            let res = mask_draw_ctx.with_discrete_layer(
                 mask_node,
                 acquired_nodes,
                 values,
                 false,
                 &mut |an, dc| mask_node.draw_children(an, &cascaded, dc, false),
             );
-
-            self.pop_cairo_context();
 
             res?;
         }
@@ -667,36 +677,40 @@ impl DrawingCtx {
 
                 cr.set_matrix(affines.for_temporary_surface.into());
 
-                saved_cr.draw_ctx.push_cairo_context(cr);
-
-                // Draw!
-
-                let mut res = draw_fn(acquired_nodes, saved_cr.draw_ctx);
-
-                let bbox = if let Ok(ref bbox) = res {
-                    *bbox
-                } else {
-                    BoundingBox::new().with_transform(affines.for_temporary_surface)
-                };
-
-                // Filter
-
                 let node_name = format!("{}", node);
 
-                let surface_to_filter = SharedImageSurface::copy_from_surface(
-                    &cairo::ImageSurface::try_from(saved_cr.draw_ctx.cr.get_target()).unwrap(),
-                )?;
+                let (source_surface, mut res, bbox) = {
+                    let mut temporary_draw_ctx = saved_cr.draw_ctx.nested(cr);
 
-                let source_surface = saved_cr.draw_ctx.run_filters(
-                    surface_to_filter,
-                    &filters,
-                    acquired_nodes,
-                    &node_name,
-                    values,
-                    bbox,
-                )?;
+                    // Draw!
 
-                saved_cr.draw_ctx.pop_cairo_context();
+                    let res = draw_fn(acquired_nodes, &mut temporary_draw_ctx);
+
+                    let bbox = if let Ok(ref bbox) = res {
+                        *bbox
+                    } else {
+                        BoundingBox::new().with_transform(affines.for_temporary_surface)
+                    };
+
+                    // Filter
+
+                    let surface_to_filter = SharedImageSurface::copy_from_surface(
+                        &cairo::ImageSurface::try_from(temporary_draw_ctx.cr.get_target()).unwrap(),
+                    )?;
+
+                    (
+                        temporary_draw_ctx.run_filters(
+                            surface_to_filter,
+                            &filters,
+                            acquired_nodes,
+                            &node_name,
+                            values,
+                            bbox,
+                        )?,
+                        res,
+                        bbox,
+                    )
+                };
 
                 // Set temporary surface as source
 

commit 44927afa05ece56341f08872393332202482f951

(#823): Fix geometry computation for individual layers
(#823) correctly points out that commit
e3bb911b2cd860e4e4671c87ea9712913bb35d26 broke geometry computation.
I didn't see how it could, since it didn't even have to do with
extracting bounding boxes from elements.

It turns out that I forgot to copy the DrawingCtx.drawsub_stack for
the nested context.  So, for an SVG with structure

  a
    b
      c -> with isolation context (due to non-100% opacity / etc.)
        d
        e
        f id="draw_only_this_one"
        g

if one calls geometry_for_layer("draw_only_this_one"), draw_tree() would
properly create a drawsub_stack like (a b c f), but then the nested
rendering would go:

  stack_top=a

  stack_top=b

  stack_top=c (creates isolation context, and a nested DrawingCtx)

  stack_top=empty (nested DrawingCtx has an empty drawsub_stack) ->
    draw every child of c since there's no drawsub_stack to limit it,
    instead of just drawing f in there.

https://gitlab.gnome.org/GNOME/librsvg/-/issues/823

Part-of: <https://gitlab.gnome.org/GNOME/librsvg/-/merge_requests/697>

src/drawing_ctx.rs
@@ -326,7 +326,7 @@ impl DrawingCtx {
             cr,
             user_language: self.user_language.clone(),
             viewport_stack: self.viewport_stack.clone(),
-            drawsub_stack: Vec::new(),
+            drawsub_stack: self.drawsub_stack.clone(),
             measuring: self.measuring,
             testing: self.testing,
         }