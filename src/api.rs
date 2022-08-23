aaaa42debea82309090cf493f7b61b4f5fe7c7c8
Federico Mena Quintero <federico@gnome.org>
2022-08-22 19:22:04 -0500

    api.rs: mark the SvgHandle._session as an unused field
    It's not used yet, but it will be when we start logging API calls.
    
    Part-of: <https://gitlab.gnome.org/GNOME/librsvg/-/merge_requests/731>

--- src/api.rs
+++ src/api.rs
@@ -218,7 +218,7 @@ impl Loader {
                 stream.as_ref(),
                 cancellable.map(|c| c.as_ref()),
             )?,
-            session: self.session,
+            _session: self.session,
         })
     }
 }
@@ -232,7 +232,7 @@ fn url_from_file(file: &gio::File) -> Result<Url, LoadingError> {
 /// You can create this from one of the `read` methods in
 /// [`Loader`].
 pub struct SvgHandle {
-    session: Session,
+    _session: Session,
     pub(crate) handle: Handle,
 }
 
