52d2c9c266da93c1091aa1159088c04400eb08da
Federico Mena Quintero <federico@gnome.org>
2023-08-11 12:11:21 -0600

    Don't discard the error information from parsing cssparser::Color
    Part-of: <https://gitlab.gnome.org/GNOME/librsvg/-/merge_requests/875>

--- rsvg/src/color.rs
+++ rsvg/src/color.rs
@@ -7,9 +7,34 @@ use crate::parsers::Parse;
 
 pub use cssparser::Color;
 
+/// Turn a short-lived [`cssparser::ParseError`] into a long-lived [`ParseError`].
+///
+/// cssparser's error type has a lifetime equal to the string being parsed.  We want
+/// a long-lived error so we can store it away if needed.  Basically, here we turn
+/// a `&str` into a `String`.
 fn map_color_parse_error(err: cssparser::ParseError<'_, ()>) -> ParseError<'_> {
+    let string_err = match err.kind {
+        ParseErrorKind::Basic(ref e) => format!("{}", e),
+        ParseErrorKind::Custom(()) => {
+            // In cssparser 0.31, the error type for Color::parse is defined like this:
+            //
+            //   pub fn parse<'i>(input: &mut Parser<'i, '_>) -> Result<Color, ParseError<'i, ()>> {
+            //
+            // The ParseError<'i, ()> means that the ParseErrorKind::Custom(T) variant will have
+            // T be the () type.
+            //
+            // So, here we match for () inside the Custom variant.  If cssparser
+            // changes its error API, this match will hopefully catch errors.
+            //
+            // Implementation detail: Color::parse() does not ever return Custom errors, only
+            // Basic ones.  So the match for Basic above handles everything, and this one
+            // for () is a dummy case.
+            "could not parse color".to_string()
+        }
+    };
+
     ParseError {
-        kind: ParseErrorKind::Custom(ValueErrorKind::parse_error("Could not parse color")),
+        kind: ParseErrorKind::Custom(ValueErrorKind::Parse(string_err)),
         location: err.location,
     }
 }
