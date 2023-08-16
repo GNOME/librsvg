e7fce161c73c21d50e5181224445b352ed8335bc
Federico Mena Quintero <federico@gnome.org>
2023-08-16 16:47:50 -0600

    Remove "impl Parse for RGBA" now that it is unused
    Part-of: <https://gitlab.gnome.org/GNOME/librsvg/-/merge_requests/875>

--- rsvg/src/color.rs
+++ rsvg/src/color.rs
@@ -43,20 +43,6 @@ impl Parse for cssparser::Color {
     }
 }
 
-impl Parse for cssparser::RGBA {
-    fn parse<'i>(parser: &mut Parser<'i, '_>) -> Result<cssparser::RGBA, ParseError<'i>> {
-        let loc = parser.current_source_location();
-
-        match cssparser::Color::parse(parser).map_err(map_color_parse_error)? {
-            cssparser::Color::Rgba(rgba) => Ok(rgba),
-            cssparser::Color::CurrentColor => Err(loc.new_custom_error(ValueErrorKind::Value(
-                "currentColor is not allowed here".to_string(),
-            ))),
-            _ => Err(loc.new_custom_error(ValueErrorKind::value_error("Unsupported color type"))),
-        }
-    }
-}
-
 pub fn color_to_rgba(color: &Color) -> RGBA {
     match color {
         Color::Rgba(rgba) => *rgba,
