18bced57a5cb8982d783a14821d1448a2ade01d9
Federico Mena Quintero <federico@gnome.org>
2023-08-30 15:31:02 -0600

    CI: don't install system packages for python modules; use a venv/pip
    We put CI-specific stuff in /usr/local/python, and the ci/env.sh
    script now sources the venv's "activate" script as well.
    
    It seems that something in openSUSE's python311 update has caused pip
    to start complaining about modifying the system installation.  So, we
    use a proper venv now.

--- ci/env.sh
+++ ci/env.sh
@@ -1,3 +1,12 @@
+# Activate the Python virtual environment for CI scripts.
+#
+# We test for the presence of the file, since when first creating the container images for CI,
+# the venv has not been created yet.  This is mostly a hack to allow having a single "env.sh"
+# script instead of one for container creation and one for CI jobs.
+if [ -f /usr/local/python/bin/activate ]; then
+    source /usr/local/python/bin/activate
+fi
+
 export RUSTUP_HOME='/usr/local/rustup'
 export PATH=$PATH:/usr/local/cargo/bin
 
