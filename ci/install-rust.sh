9ee64f5a9af95f91c80e6a4a58d6a1dc9bc5e8ea
Federico Mena Quintero <federico@gnome.org>
2023-09-19 21:01:59 -0600

    install-rust.sh: take all parameters as arguments, not as environment variables
    Part-of: <https://gitlab.gnome.org/GNOME/librsvg/-/merge_requests/896>

--- ci/install-rust.sh
+++ ci/install-rust.sh
@@ -1,22 +1,73 @@
+#!/bin/bash
+
+set -o errexit -o pipefail -o noclobber -o nounset
+
 source ./ci/env.sh
 
-set -eu
 export CARGO_HOME='/usr/local/cargo'
 
-if [ -z "$RUSTUP_VERSION" ]
-then
-    echo "RUSTUP_VERSION is not set, please set it"
+PARSED=$(getopt --options '' --longoptions 'rustup-version:,version:,arch:' --name "$0" -- "$@")
+if [ $? -ne 0 ]; then
+	echo 'Terminating...' >&2
+	exit 1
+fi
+
+eval set -- "$PARSED"
+unset PARSED
+
+RUSTUP_VERSION=
+VERSION=
+ARCH=
+
+while true; do
+    case "$1" in
+        '--rustup-version')
+            RUSTUP_VERSION=$2
+            shift 2
+            ;;
+
+        '--version')
+            VERSION=$2
+            shift 2
+            ;;
+
+        '--arch')
+            ARCH=$2
+            shift 2
+            ;;
+
+        '--')
+            shift
+            break
+            ;;
+
+        *)
+            echo "Programming error"
+            exit 3
+            ;;
+    esac
+done
+
+if [ -z "$RUSTUP_VERSION" ]; then
+    echo "missing --rustup-version argument"
     exit 1
 fi
 
-RUST_VERSION=$1
-RUST_ARCH=$2
+if [ -z "$VERSION"]; then
+    echo "missing --version argument, please pass the version of rustc you want"
+    exit 1
+fi
+    
+if [ -z "$ARCH"]; then
+    echo "missing --arch argument, please pass an architecture triple like x86_64-unknown-linux-gnu"
+    exit 1
+fi
 
-RUSTUP_URL=https://static.rust-lang.org/rustup/archive/$RUSTUP_VERSION/$RUST_ARCH/rustup-init
+RUSTUP_URL=https://static.rust-lang.org/rustup/archive/$RUSTUP_VERSION/$ARCH/rustup-init
 wget $RUSTUP_URL
 
 chmod +x rustup-init;
-./rustup-init -y --no-modify-path --profile minimal --default-toolchain $RUST_VERSION;
+./rustup-init -y --no-modify-path --profile minimal --default-toolchain $VERSION;
 rm rustup-init;
 chmod -R a+w $RUSTUP_HOME $CARGO_HOME
 
