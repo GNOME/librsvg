5fe7ef96139b658fcfbf17ea2cd49f041dd4f6c4
L. E. Segovia <amy@centricular.com>
2024-02-23 18:10:55 -0300

    ci: Simplify the MSYS2 testing script with pacboy
    Co-authored-by: Rachel Mant <git@dragonmux.network>

--- ci/test-msys2.sh
+++ ci/test-msys2.sh
@@ -12,22 +12,25 @@ pacman --noconfirm -Suy
 
 pacman --noconfirm -S --needed \
     base-devel \
-    mingw-w64-$MSYS2_ARCH-meson \
-    mingw-w64-$MSYS2_ARCH-cargo-c \
-    mingw-w64-$MSYS2_ARCH-gi-docgen \
-    mingw-w64-$MSYS2_ARCH-gobject-introspection \
-    mingw-w64-$MSYS2_ARCH-gdk-pixbuf2 \
-    mingw-w64-$MSYS2_ARCH-harfbuzz \
-    mingw-w64-$MSYS2_ARCH-fontconfig \
-    mingw-w64-$MSYS2_ARCH-fribidi \
-    mingw-w64-$MSYS2_ARCH-libthai \
-    mingw-w64-$MSYS2_ARCH-cairo \
-    mingw-w64-$MSYS2_ARCH-pango \
-    mingw-w64-$MSYS2_ARCH-python-docutils \
-    mingw-w64-$MSYS2_ARCH-libxml2 \
-    mingw-w64-$MSYS2_ARCH-toolchain \
-    mingw-w64-$MSYS2_ARCH-rust \
-    mingw-w64-$MSYS2_ARCH-cantarell-fonts
+    pactoys
+
+pacboy --noconfirm -S --needed \
+    meson:p \
+    cargo-c:p \
+    gi-docgen:p \
+    gobject-introspection:p \
+    gdk-pixbuf2:p \
+    harfbuzz:p \
+    fontconfig:p \
+    fribidi:p \
+    libthai:p \
+    cairo:p \
+    pango:p \
+    python-docutils:p \
+    libxml2:p \
+    toolchain:p \
+    rust:p \
+    cantarell-fonts:p
 
 meson setup _build -Dauto_features=disabled
 meson compile -C _build
