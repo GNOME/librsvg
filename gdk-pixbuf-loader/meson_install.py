c95b5df57f2a76311b065aff0f245d48f44f90ec
Chun-wei Fan <fanc999@yahoo.com.tw>
2024-02-29 16:52:10 +0800

    Meson: Show message regarding installing svg loader for cross builds
    Tell people to manually run gdk-pixbuf-query-loaders on the target system
    against the newly-built svg pixbuf loader, if the build is a cross build.
    
    Part-of: <https://gitlab.gnome.org/GNOME/librsvg/-/merge_requests/932>

--- gdk-pixbuf-loader/meson_install.py
+++ gdk-pixbuf-loader/meson_install.py
@@ -2,14 +2,16 @@
 
 from argparse import ArgumentParser
 from pathlib import Path
-import subprocess
 import os
+import subprocess
+import sys
 
 argparse = ArgumentParser('Deploy loaders.cache')
 
 argparse.add_argument('--gdk-pixbuf-moduledir', type=Path)
 argparse.add_argument('gdk_pixbuf_queryloaders', type=Path)
 argparse.add_argument('gdk_pixbuf_cache_file', type=Path)
+argparse.add_argument('--show-cross-message', action='store_true')
 
 if __name__ == '__main__':
     args = argparse.parse_args()
@@ -24,13 +26,17 @@ if __name__ == '__main__':
         assert destdir is not None
         cache_file = destdir / cache_file.relative_to(cache_file.anchor)
 
-    with cache_file.open('w', encoding='utf-8') as f:
-        subprocess.run(
-            [args.gdk_pixbuf_queryloaders],
-            env={
-                'GDK_PIXBUF_MODULEDIR': args.gdk_pixbuf_moduledir
-            },
-            stdout=f,
-            check=True
-        )
+    if args.show_cross_message:
+        print('*** Note: Please run gdk-pixbuf-queryloaders manually ' +
+              'against the newly-built gdkpixbuf-svg loader', file=sys.stderr)
+    else:
+        with cache_file.open('w', encoding='utf-8') as f:
+            subprocess.run(
+                [args.gdk_pixbuf_queryloaders],
+                env={
+                    'GDK_PIXBUF_MODULEDIR': args.gdk_pixbuf_moduledir
+                },
+                stdout=f,
+                check=True
+            )
 
