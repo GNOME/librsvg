fe71fd4b2be35847df96d17180d40a67b74b343a
Chun-wei Fan <fanc999@yahoo.com.tw>
2024-03-18 10:49:53 +0800

    meson: Split native static lib query out
    Use a new argument to query-rustc.py to specify that we are querying the
    native static libraries for the build, and update that script
    accordingly, so that we will pass into the script to query for the items
    that we need, and grab the needed items from the output accordingly.

--- meson/query-rustc.py
+++ meson/query-rustc.py
@@ -11,6 +11,9 @@ import tempfile
 parser = ArgumentParser()
 
 parser.add_argument("RUSTC", type=Path, help="Path to rustc")
+parser.add_argument("--query", action="store",
+                    choices=['native-static-libs'], help="Item to query from RustC ['native-static-libs']",
+                    required=True)
 
 def removeprefix_fallback(s, pfx):
     if sys.version_info > (3, 9):
@@ -28,29 +31,8 @@ def removesuffix_fallback(s, sfx):
     else:
         return s
 
-parser.add_argument("--target", help="Target triplet")
-
-if __name__ == "__main__":
-    args = parser.parse_args()
-    dummy_out = tempfile.NamedTemporaryFile()
-
-    rustc_cmd = [
-        Path(args.RUSTC).as_posix(),
-        "--print=native-static-libs",
-        "--crate-type", "staticlib"
-    ]
-    if args.target:
-        rustc_cmd.extend(['--target', args.target])
-
-    rustc_cmd.append(os.devnull)
-    rustc_cmd.extend(['-o', dummy_out.name])
-
-    native_static_libs = subprocess.run(
-        rustc_cmd,
-        capture_output=True,
-        text=True,
-    )
-    for i in native_static_libs.stderr.strip().splitlines():
+def retrieve_native_static_libs_from_output(output):
+    for i in output.strip().splitlines():
         match = re.match(r".+native-static-libs: (.+)", i)
         if match:
             libs = match.group(1).split()
@@ -63,3 +45,31 @@ if __name__ == "__main__":
                     )
                 )
             )
+
+parser.add_argument("--target", help="Target triplet")
+
+if __name__ == "__main__":
+    args = parser.parse_args()
+    dummy_out = tempfile.NamedTemporaryFile()
+    query = args.query
+
+    rustc_cmd = [
+        Path(args.RUSTC).as_posix(),
+        "--print=%s" % query,
+    ]
+    if args.target:
+        rustc_cmd.extend(['--target', args.target])
+
+    # We need these for '--print=native-static-libs' on Windows
+    if query == 'native-static-libs':
+        rustc_cmd.extend(['--crate-type', 'staticlib'])
+        rustc_cmd.append(os.devnull)
+        rustc_cmd.extend(['-o', dummy_out.name])
+
+    query_results = subprocess.run(
+        rustc_cmd,
+        capture_output=True,
+        text=True,
+    )
+    if query == 'native-static-libs':
+        retrieve_native_static_libs_from_output(query_results.stderr)
