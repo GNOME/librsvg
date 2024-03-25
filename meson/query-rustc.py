98402d468f4be9fcc04eb09fc1326a16d1d85acc
Chun-wei Fan <fanc999@yahoo.com.tw>
2024-03-25 13:00:19 +0800

    meson: Check against installed stable toolchain too
    If one requests a toolchain release version, check against the installed
    stable toolchain release version as well, as Rust does not do that for
    us but we need to check against that manually.
    
    Part-of: <https://gitlab.gnome.org/GNOME/librsvg/-/merge_requests/936>

--- meson/query-rustc.py
+++ meson/query-rustc.py
@@ -12,8 +12,8 @@ parser = ArgumentParser()
 
 parser.add_argument("RUSTC", type=Path, help="Path to rustc")
 parser.add_argument("--query", action="store",
-                    choices=['native-static-libs', 'default-host-toolchain'],
-                    help="Item to query from RustC ['native-static-libs', 'default-host-toolchain']",
+                    choices=['native-static-libs', 'default-host-toolchain', 'stable-actual-version'],
+                    help="Item to query from RustC ['native-static-libs', 'default-host-toolchain', 'stable-actual-version']",
                     required=True)
 parser.add_argument("--toolchain-version", action="store",
                     help="Rust Toolchain Version (if needed)")
@@ -55,12 +55,12 @@ def retrieve_native_static_libs_from_output(output):
                 )
             )
 
-# Get the default target host
-def retrieve_default_host_toolchain(output):
+# Get the default target host or actual version of toolchain
+def retrive_version_info(output, query):
     for i in output.strip().splitlines():
-        match = re.match(r"host: (.+)", i)
+        match = re.match(r"%s: (.+)" % query, i)
         if match:
-            print(match.group(1))
+            return match.group(1)
 
 if __name__ == "__main__":
     args = parser.parse_args()
@@ -98,8 +98,12 @@ if __name__ == "__main__":
     )
     if query == 'native-static-libs':
         retrieve_native_static_libs_from_output(query_results.stderr)
-    elif query == 'default-host-toolchain':
+    elif query == 'default-host-toolchain' or query == 'stable-actual-version':
         if query_results.stderr == '':
-            retrieve_default_host_toolchain(query_results.stdout)
+            if query == 'default-host-toolchain':
+                result = retrive_version_info(query_results.stdout, 'host')
+            else:
+                result = retrive_version_info(query_results.stdout, 'release')
+            print(result)
         else:
             print(query_results.stderr, file=sys.stderr)
