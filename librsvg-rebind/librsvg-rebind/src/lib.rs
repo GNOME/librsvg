5aa456de78f95ecf2feb56bbee10a34c1cc0209b
Sophie Herold <sophie@hemio.de>
2024-08-01 16:30:21 +0200

    librsvg-rebind: Add more info and minimal example
    Part-of: <https://gitlab.gnome.org/GNOME/librsvg/-/merge_requests/998>

--- librsvg-rebind/librsvg-rebind/src/lib.rs
+++ librsvg-rebind/librsvg-rebind/src/lib.rs
@@ -3,7 +3,39 @@
 #![doc(html_logo_url = "https://gnome.pages.gitlab.gnome.org/librsvg/Rsvg-2.0/librsvg-r.svg")]
 //! # Rust librsvg bindings
 //!
-//! This library contains safe Rust bindings for librsvg
+//! This package contains safe Rust bindings for the librsvg C library.
+//!
+//! Since librsvg is written in Rust, the native [`rsvg`] crate is available
+//! to use the same features. One of the main purposes of *librsvg-rebind*
+//! is to reduce the binary sice.
+//!
+//! To use this package, the *librsvg-2* library has to be available on the system.
+//! If you use the native [`rsvg`] crate, this is not required.
+//!
+//! [`rsvg`]: https://crates.io/crates/librsvg
+//!
+//! # Example
+//!
+//! ```
+//! use librsvg_rebind::prelude::*;
+//!
+//! let handle = librsvg_rebind::Handle::from_file("../../rsvg/example.svg")
+//!     .unwrap()
+//!     .unwrap();
+//!
+//! let (width, height) = handle.intrinsic_size_in_pixels().unwrap();
+//!
+//! let surface =
+//!     cairo::ImageSurface::create(cairo::Format::ARgb32, width as i32, height as i32).unwrap();
+//! let context = cairo::Context::new(&surface).unwrap();
+//!
+//! let viewport = librsvg_rebind::Rectangle::new(0., 0., height, width);
+//!
+//! handle.render_document(&context, &viewport).unwrap();
+//!
+//! let mut output_file = std::fs::File::create("/dev/null").unwrap();
+//! surface.write_to_png(&mut output_file).unwrap();
+//! ```
 
 /// No-op.
 macro_rules! skip_assert_initialized {
