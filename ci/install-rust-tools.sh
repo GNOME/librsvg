d271d34d5e921700b3224ef511edf167db76490b
Federico Mena Quintero <federico@gnome.org>
2024-09-11 18:08:15 -0600

    Try "cargo install --locked" for cargo-c

--- ci/install-rust-tools.sh
+++ ci/install-rust-tools.sh
@@ -5,7 +5,7 @@ export CARGO_HOME='/usr/local/cargo'
 
 rustup component add clippy
 rustup component add rustfmt
-cargo install --force cargo-c
+cargo install --force --locked cargo-c
 cargo install --version ^1.0 gitlab_clippy
 cargo install --force cargo-deny
 # cargo install --force cargo-outdated
