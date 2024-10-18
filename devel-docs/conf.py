822b276c36e1fe78753a3edd817d0616b60e5770
Toluwaleke Ogundipe <anonymoux47@gmail.com>
2024-10-18 17:40:51 +0100

    devel-docs: Implement `internals` Sphinx extension
    This Sphinx extension simplifies and improves referencing of entities
    in the librsvg internals documentation.
    
    Part-of: <https://gitlab.gnome.org/GNOME/librsvg/-/merge_requests/1041>

--- devel-docs/conf.py
+++ devel-docs/conf.py
@@ -9,10 +9,9 @@
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
-#
-# import os
-# import sys
-# sys.path.insert(0, os.path.abspath('.'))
+import os
+import sys
+sys.path.insert(0, os.path.abspath('_extensions'))
 
 
 # -- Project information -----------------------------------------------------
@@ -31,6 +30,9 @@ extensions = [
     # Used to link issues, merge requests, CVEs, etc.
     # https://github.com/sloria/sphinx-issues
     "sphinx_issues",
+    # Used to reference entities in the internals documentation.
+    # ./_extensions/internals.py
+    "internals",
 ]
 
 # Add any paths that contain templates here, relative to this directory.
