731876dfc48c2adaf79ecf2756d0adddeab89753
Federico Mena Quintero <federico@gnome.org>
2025-09-11 18:15:15 -0600

    Since we have a newer rustc, use cargo-deny 0.18.3.
    It's not the latest, but it will build with Rust 1.85.

--- ci/install-rust-tools.sh
+++ ci/install-rust-tools.sh
@@ -7,4 +7,4 @@ rustup component add clippy
 rustup component add rustfmt
 # cargo install --force cargo-c
 # cargo install --version ^1.0 gitlab_clippy
-cargo install --force cargo-deny --locked --version 0.14.18
+cargo install --force cargo-deny --locked --version 0.18.3
