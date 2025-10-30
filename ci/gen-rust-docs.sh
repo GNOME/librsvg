55208181e4b84345319b0935fcdc3cbf8e7f756f
Federico Mena Quintero <federico@gnome.org>
2025-10-29 19:55:19 -0600

    (#1207): CI: Enable the cargo-doc job again
    Disable a full-workspace docs build; for the published docs we only
    need the librsvg crate.
    
    I'll disable the docs for the librsvg-rebind crate for now; I don't
    think they contain anything...?
    
    Fixes https://gitlab.gnome.org/GNOME/librsvg/-/issues/1207
    
    Part-of: <https://gitlab.gnome.org/GNOME/librsvg/-/merge_requests/1149>

--- ci/gen-rust-docs.sh
+++ ci/gen-rust-docs.sh
@@ -6,10 +6,11 @@
 
 set -eu
 
-cargo doc --workspace --document-private-items --no-deps
+cargo doc --document-private-items --no-deps --package librsvg
 mkdir -p public/internals
 mv target/doc/* public/internals
 
-cargo doc --no-deps --package librsvg --package 'librsvg-rebind*'
+# cargo doc --no-deps --package librsvg --package 'librsvg-rebind*'
+cargo doc --no-deps --package librsvg
 mkdir -p public/doc
 cp -r target/doc/* public/doc
