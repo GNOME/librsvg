ad8ea361606ae48956bfb0b123d6a8d433f48bee
Federico Mena Quintero <federico@gnome.org>
2026-02-10 12:45:19 -0600

    Don't use a 'gen' keyword
    Part-of: <https://gitlab.gnome.org/GNOME/librsvg/-/merge_requests/1174>

--- rsvg_convert/src/main.rs
+++ rsvg_convert/src/main.rs
@@ -809,8 +809,8 @@ fn natural_geometry(
     })
 }
 
-fn print_completions<G: Generator>(gen: G, cmd: &mut clap::Command) {
-    clap_complete::generate(gen, cmd, cmd.get_name().to_string(), &mut io::stdout());
+fn print_completions<G: Generator>(generator: G, cmd: &mut clap::Command) {
+    clap_complete::generate(generator, cmd, cmd.get_name().to_string(), &mut io::stdout());
 }
 
 unsafe extern "C" {
