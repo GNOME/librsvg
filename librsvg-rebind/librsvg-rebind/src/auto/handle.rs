352a6d78c273a7c25b13e882257a873ac50cab18
Federico Mena Quintero <federico@gnome.org>
2026-02-27 17:54:09 -0600

    cargo fmt
    Part-of: <https://gitlab.gnome.org/GNOME/librsvg/-/merge_requests/1182>

--- librsvg-rebind/librsvg-rebind/src/auto/handle.rs
+++ librsvg-rebind/librsvg-rebind/src/auto/handle.rs
@@ -4,10 +4,10 @@
 // DO NOT EDIT
 #![allow(deprecated)]
 
-use crate::{ffi, HandleFlags, Rectangle};
+use crate::{HandleFlags, Rectangle, ffi};
 use glib::{
     prelude::*,
-    signal::{connect_raw, SignalHandlerId},
+    signal::{SignalHandlerId, connect_raw},
     translate::*,
 };
 use std::boxed::Box as Box_;
