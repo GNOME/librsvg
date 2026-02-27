6050bab03a2a8089aaa44c7f5c3256e1ce0e1611
Federico Mena Quintero <federico@gnome.org>
2026-02-27 14:43:52 -0600

    Bump version to 2.62.0
    Part-of: <https://gitlab.gnome.org/GNOME/librsvg/-/merge_requests/1182>

--- rsvg/src/lib.rs
+++ rsvg/src/lib.rs
@@ -20,7 +20,7 @@
 //!
 //! ```toml
 //! [dependencies]
-//! librsvg = "2.62.0-beta.2"
+//! librsvg = "2.62.0"
 //! cairo-rs = "0.22.0-alpha"
 //! gio = "0.22.0-alpha"   # only if you need streams
 //! ```
