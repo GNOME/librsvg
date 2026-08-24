93c6c2361f701f88d566e6bc43cd7189a2feb00a
Federico Mena Quintero <federico@gnome.org>
2026-08-24 14:00:19 -0600

    Explain how the librsvg-c version number gets generated for the public symbols
    Part-of: <https://gitlab.gnome.org/GNOME/librsvg/-/merge_requests/1207>

--- librsvg-c/build.rs
+++ librsvg-c/build.rs
@@ -1,3 +1,18 @@
+//! Generate the public symbols that contain the library version
+//!
+//! The C API/ABI of librsvg exports a few `no_mangle` symbols which contain the library's
+//! version number.  The code in this file does this:
+//!
+//! 1. Read the main `meson.build` from the source tree, which contains the version number.
+//!
+//! 2. Scan the file for a string like `version: '11.22.33'`.
+//!
+//! 3. Generate a `version.rs` source file to be included from the librsvg-c crate, with
+//!    the appropriate numbers for the version.
+//!
+//! If you want to find the place in the source code where this generated code gets included,
+//! look for the `version.rs` string in `librsvg/src`.
+
 use regex::Regex;
 use std::env;
 use std::fs::File;
