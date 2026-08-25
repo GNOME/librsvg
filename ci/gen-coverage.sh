031fa3f94452a1aae9c318f21b22c97c0d87a746
Federico Mena Quintero <federico@gnome.org>
2026-08-25 13:30:02 -0600

    Increase the maximum size of the coverage XML report to 20 MB
    It's slightly over 10 MB now, but it's not huge, so let's allow some
    room for extra growth.
    
    Part-of: <https://gitlab.gnome.org/GNOME/librsvg/-/merge_requests/1207>

--- ci/gen-coverage.sh
+++ ci/gen-coverage.sh
@@ -42,10 +42,10 @@ call_grcov html public/coverage
 
 call_grcov cobertura coverage.xml
 size=$(wc -c < coverage.xml)
-if [ "$size" -ge 10485760 ]
+if [ "$size" -ge 20971520 ]
 then
     rm coverage.xml
-    echo "coverage.xml is over 10 MB, removing it so it will not be used"
+    echo "coverage.xml is over 20 MB, removing it so it will not be used"
 fi
 
 # Print "Coverage: 42.42" so .gitlab-ci.yml will pick it up with a regex
