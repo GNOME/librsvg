1ac6adf80bfc05b9bb23f80ede2756d384407d9d
Federico Mena Quintero <federico@gnome.org>
2026-08-25 20:03:24 -0600

    Fix generation of test coverage data for the C API code in librsvg-c
    Don't use meson's coverage option; use clang's options directly.
    
    We explicitly do not use meson's -Db_coverage=true option anymore.
    That option causes meson to add --coverage to clang's options, which
    makes it use GCC-compatible gcov output.  Instead, we use the clang
    options -fprofile-instr-generate -fcoverage-mapping, per
    https://clang.llvm.org/docs/SourceBasedCodeCoverage.html
    
    Somehow, if we use GCC-compatible gcov output (via meson's
    -Db_coverage=true), the profile data
    for librsvg-c is not captured when grcov calls llvm-cov internally:
    running grcov with --log-level=TRACE shows a bunch of these:
    
      23:19:58 [WARN] Suppressing error returned by llvm-cov tool for binary "./_build/librsvg-c/tests-c/api.p/api.c.o"
    
      Failure while running
      "/usr/local/rustup/toolchains/1.92.0-x86_64-unknown-linux-gnu/lib/rustlib/x86_64-unknown-linux-gnu/bin/llvm-cov"
      "export" "./_build/librsvg-c/tests-c/api.p/api.c.o" "--instr-profile"
      "/tmp/.tmplXtaUD/0/grcov.profdata" "--format" "lcov"
    
      error: failed to load coverage: './_build/librsvg-c/tests-c/api.p/api.c.o': no coverage data found
      error: could not load coverage information
    
    Using clang's options explicitly via CFLAGS causes this to work
    correctly.
    
    Part-of: <https://gitlab.gnome.org/GNOME/librsvg/-/merge_requests/1207>

--- ci/build-with-coverage.sh
+++ ci/build-with-coverage.sh
@@ -13,16 +13,15 @@ then
     exit 1
 fi
 
-# Mixed gcc and Rust/LLVM coverage for the C API tests:
-# https://searchfox.org/mozilla-central/source/browser/config/mozconfigs/linux64/code-coverage#15
 export CC="clang"
+export CFLAGS="-fprofile-instr-generate -fcoverage-mapping"
 export RUSTDOCFLAGS="-C instrument-coverage"
 LLVM_PROFILE_FILE="$(pwd)/coverage-profiles/coverage-%p-%m.profraw"
 export LLVM_PROFILE_FILE
 export RUSTC_BOOTSTRAP="1"   # hack to make unstable options work on the non-nightly compiler
 export RUSTFLAGS="-C instrument-coverage -Z coverage-options=condition -Ccodegen-units=1 -Clink-dead-code -Coverflow-checks=off"
 
-meson setup _build -Db_coverage=true -Dauto_features=disabled -Dpixbuf{,-loader}=enabled --buildtype=debugoptimized
+meson setup _build -Dauto_features=disabled -Dpixbuf{,-loader}=enabled --buildtype=debugoptimized
 meson compile -C _build
 meson test -C _build --maxfail 0 --print-errorlogs
 
