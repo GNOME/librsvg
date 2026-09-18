03a6a1f963a78d3e7d5cc398953d3ddc74e3a137
Federico Mena Quintero <federico@gnome.org>
2026-09-18 12:21:47 -0600

    Remove obsolete comment
    Part-of: <https://gitlab.gnome.org/GNOME/librsvg/-/merge_requests/1211>

--- rsvg/src/xml/mod.rs
+++ rsvg/src/xml/mod.rs
@@ -129,14 +129,6 @@ struct XmlStateInner {
     xinclude_depth: usize,
     context_stack: Vec<Context>,
     current_node: Option<Node>,
-
-    // Note that neither XmlStateInner nor Xmlstate implement Drop.
-    //
-    // An XmlState is finally consumed in XmlState::build_document(), and that
-    // function is responsible for freeing all the XmlEntityPtr from this field.
-    //
-    // (The structs cannot impl Drop because build_document()
-    // destructures and consumes them at the same time.)
     entities: HashMap<String, XmlEntity>,
 }
 
